aac51e6663b8a76e8140a11004c6542c194a68ac
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-06 00:13:33 +0900

    update data

--- gqlgen/create-data.sh
+++ gqlgen/create-data.sh
@@ -7,10 +7,10 @@ for j in 0 1 2 3 4 5 6 7 8 9
 do
   for i in 0 1 2 3 4 5 6 7 8 9
   do
-    FILENAME="$BASEDIR/step$j$i/state.json"
-    if [ -f $FILENAME ]
+    FILENAME="$BASEDIR/step$j$i.json"
+    if [ ! -f "$FILENAME" ]
     then
-      mv "$FILENAME" "$BASEDIR/step$j$i.json"
+      echo "" > "$FILENAME"
     fi
   done
 done
