6f3d6856612e08a89449c03e1c635b2923a2e633
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-07 06:37:36 +0900

    actuall prism error import

--- next/components/IDE.tsx
+++ next/components/IDE.tsx
@@ -1,4 +1,8 @@
 import { css } from "@emotion/react";
+import Prism from "prismjs";
+import "prismjs/components/prism-protobuf"; //ts 7016 error suppressed by prism-fix.d.ts
+import "prismjs/themes/prism-tomorrow.css";
+import { useEffect, useRef } from "react";
 import { IDEEditorTab } from "./IDEEditorTab";
 import { IDESideBar } from "./IDESideBar";
 
@@ -21,6 +25,12 @@ message HelloReply {
 }`;
 
 export const IDE = (): JSX.Element => {
+  const ref = useRef<HTMLElement>(null);
+  useEffect(() => {
+    if (ref.current) {
+      Prism.highlightElement(ref.current);
+    }
+  }, []);
   return (
     <div
       css={css`
@@ -32,7 +42,16 @@ export const IDE = (): JSX.Element => {
         <IDEEditorTab filename="package.json" />
         <div>
           <pre>
-            <code>{sourceCode}</code>
+            <code
+              style={{ fontSize: "12px" }}
+              // css={css`
+              //   font-size: 12px;
+              // `}
+              className="language-protobuf"
+              ref={ref}
+            >
+              {sourceCode}
+            </code>
           </pre>
         </div>
       </div>
