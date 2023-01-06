3079ede9ccb14b9caaf1040a628fa58d5e045692
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-07 08:30:46 +0900

    shrink button

--- next/components/IDE.tsx
+++ next/components/IDE.tsx
@@ -50,10 +50,22 @@ export const IDE = (): JSX.Element => {
       >
         <div
           css={css`
-            height: 28px;
+            display: flex;
+            padding: 6px 10px 6px 6px;
+            justify-content: end;
+            background-color: #222121;
           `}
         >
-          {"<<"}
+          <img
+            width="16"
+            height="16"
+            css={css`
+              display: block;
+              background-color: #f7f7f7;
+              border-radius: 2px;
+            `}
+            src="/images/ide-sidebar-shrink.svg"
+          />
         </div>
         <IDESideBar height={sourceCodeHeight} />
       </div>
--- next/components/IDESideBar.tsx
+++ next/components/IDESideBar.tsx
@@ -5,44 +5,44 @@ interface IDESideBarProps {
   height: number;
 }
 
-export const IDESideBar = ({ height }: IDESideBarProps): JSX.Element => {
-  const files = [
-    { offset: 0, __typename: "directory", filename: "next" },
-    { offset: 1, __typename: "directory", filename: "cache" },
-    { offset: 1, __typename: "directory", filename: "server" },
-    { offset: 1, __typename: "directory", filename: "static" },
-    { offset: 1, __typename: "file", filename: "build-manifest.json" },
-    { offset: 1, __typename: "file", filename: "package.json" },
-    {
-      offset: 1,
-      __typename: "file",
-      filename: "react-loadable-manifest.json",
-    },
-    { offset: 1, __typename: "directory", filename: "trace" },
-    { offset: 0, __typename: "directory", filename: "components" },
-    { offset: 1, __typename: "file", filename: "Terminal.tsx" },
-    { offset: 0, __typename: "directory", filename: "node_module" },
-    { offset: 0, __typename: "directory", filename: "pages" },
-    { offset: 1, __typename: "directory", filename: "api" },
-    { offset: 2, __typename: "file", filename: "hello.ts" },
-    { offset: 1, __typename: "file", filename: "_app.tsx" },
-    { offset: 1, __typename: "file", filename: "_document.tsx" },
-    { offset: 1, __typename: "file", filename: "index.tsx" },
-    { offset: 0, __typename: "directory", filename: "public" },
-    { offset: 0, __typename: "directory", filename: "styles" },
-    { offset: 1, __typename: "file", filename: "global.css" },
-    { offset: 1, __typename: "file", filename: "Home.module.css" },
-    { offset: 1, __typename: "directory", filename: ".babelrc" },
-    { offset: 0, __typename: "file", filename: ".eslintrc.json" },
-    { offset: 0, __typename: "directory", filename: ".gitignore" },
-    { offset: 0, __typename: "file", filename: "next-env.d.ts" },
-    { offset: 0, __typename: "file", filename: "next.config.js" },
-    { offset: 0, __typename: "file", filename: "package-lock.json" },
-    { offset: 0, __typename: "file", filename: "package.json" },
-    { offset: 0, __typename: "file", filename: "README.md" },
-    { offset: 0, __typename: "file", filename: "tsconfig.json" },
-  ];
+const files = [
+  { offset: 0, __typename: "directory", filename: "next" },
+  { offset: 1, __typename: "directory", filename: "cache" },
+  { offset: 1, __typename: "directory", filename: "server" },
+  { offset: 1, __typename: "directory", filename: "static" },
+  { offset: 1, __typename: "file", filename: "build-manifest.json" },
+  { offset: 1, __typename: "file", filename: "package.json" },
+  {
+    offset: 1,
+    __typename: "file",
+    filename: "react-loadable-manifest.json",
+  },
+  { offset: 1, __typename: "directory", filename: "trace" },
+  { offset: 0, __typename: "directory", filename: "components" },
+  { offset: 1, __typename: "file", filename: "Terminal.tsx" },
+  { offset: 0, __typename: "directory", filename: "node_module" },
+  { offset: 0, __typename: "directory", filename: "pages" },
+  { offset: 1, __typename: "directory", filename: "api" },
+  { offset: 2, __typename: "file", filename: "hello.ts" },
+  { offset: 1, __typename: "file", filename: "_app.tsx" },
+  { offset: 1, __typename: "file", filename: "_document.tsx" },
+  { offset: 1, __typename: "file", filename: "index.tsx" },
+  { offset: 0, __typename: "directory", filename: "public" },
+  { offset: 0, __typename: "directory", filename: "styles" },
+  { offset: 1, __typename: "file", filename: "global.css" },
+  { offset: 1, __typename: "file", filename: "Home.module.css" },
+  { offset: 1, __typename: "directory", filename: ".babelrc" },
+  { offset: 0, __typename: "file", filename: ".eslintrc.json" },
+  { offset: 0, __typename: "directory", filename: ".gitignore" },
+  { offset: 0, __typename: "file", filename: "next-env.d.ts" },
+  { offset: 0, __typename: "file", filename: "next.config.js" },
+  { offset: 0, __typename: "file", filename: "package-lock.json" },
+  { offset: 0, __typename: "file", filename: "package.json" },
+  { offset: 0, __typename: "file", filename: "README.md" },
+  { offset: 0, __typename: "file", filename: "tsconfig.json" },
+];
 
+export const IDESideBar = ({ height }: IDESideBarProps): JSX.Element => {
   return (
     <div
       css={css`
