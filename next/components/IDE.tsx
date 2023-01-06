7b092348ff639ad2685475cd22278adb800b6505
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-07 08:35:07 +0900

    break IDE Sidebar

--- next/components/IDE.tsx
+++ next/components/IDE.tsx
@@ -5,6 +5,7 @@ import { IDESideBar } from "./IDESideBar";
 
 import Prism from "prismjs";
 import "prismjs/components/prism-protobuf"; //ts 7016 error suppressed by prism-fix.d.ts
+import { FileNameComponent } from "./IDE/filetree/FileNameComponent";
 // prism stylesheet /styles/prism-xxx.css is imported from /pages/_app.tsx, as global stylesheet import is only allowed there.
 // https://nextjs.org/docs/messages/css-global
 
@@ -27,6 +28,43 @@ message HelloReply {
 }
 
 `;
+
+const files = [
+  { offset: 0, __typename: "directory", filename: "next" },
+  { offset: 1, __typename: "directory", filename: "cache" },
+  { offset: 1, __typename: "directory", filename: "server" },
+  { offset: 1, __typename: "directory", filename: "static" },
+  { offset: 1, __typename: "file", filename: "build-manifest.json" },
+  { offset: 1, __typename: "file", filename: "package.json" },
+  {
+    offset: 1,
+    __typename: "file",
+    filename: "react-loadable-manifest.json",
+  },
+  { offset: 1, __typename: "directory", filename: "trace" },
+  { offset: 0, __typename: "directory", filename: "components" },
+  { offset: 1, __typename: "file", filename: "Terminal.tsx" },
+  { offset: 0, __typename: "directory", filename: "node_module" },
+  { offset: 0, __typename: "directory", filename: "pages" },
+  { offset: 1, __typename: "directory", filename: "api" },
+  { offset: 2, __typename: "file", filename: "hello.ts" },
+  { offset: 1, __typename: "file", filename: "_app.tsx" },
+  { offset: 1, __typename: "file", filename: "_document.tsx" },
+  { offset: 1, __typename: "file", filename: "index.tsx" },
+  { offset: 0, __typename: "directory", filename: "public" },
+  { offset: 0, __typename: "directory", filename: "styles" },
+  { offset: 1, __typename: "file", filename: "global.css" },
+  { offset: 1, __typename: "file", filename: "Home.module.css" },
+  { offset: 1, __typename: "directory", filename: ".babelrc" },
+  { offset: 0, __typename: "file", filename: ".eslintrc.json" },
+  { offset: 0, __typename: "directory", filename: ".gitignore" },
+  { offset: 0, __typename: "file", filename: "next-env.d.ts" },
+  { offset: 0, __typename: "file", filename: "next.config.js" },
+  { offset: 0, __typename: "file", filename: "package-lock.json" },
+  { offset: 0, __typename: "file", filename: "package.json" },
+  { offset: 0, __typename: "file", filename: "README.md" },
+  { offset: 0, __typename: "file", filename: "tsconfig.json" },
+];
 
 const sourceCodeHeight = 400;
 
@@ -67,7 +105,37 @@ export const IDE = (): JSX.Element => {
             src="/images/ide-sidebar-shrink.svg"
           />
         </div>
-        <IDESideBar height={sourceCodeHeight} />
+        <div
+          css={css`
+            height: ${sourceCodeHeight}px;
+            max-width: 160px;
+            overflow: auto;
+            ::-webkit-scrollbar {
+              width: 5px;
+              height: 5px;
+              background-color: #252526; /* or add it to the track */
+            }
+            ::-webkit-scrollbar-thumb {
+              background: #a0a0a0;
+              border-radius: 5px;
+            }
+          `}
+        >
+          <div
+            css={css`
+              width: fit-content;
+            `}
+          >
+            {files.map((elem) => (
+              <FileNameComponent
+                key={elem.filename}
+                type={elem.__typename}
+                offset={elem.offset}
+                name={elem.filename}
+              />
+            ))}
+          </div>
+        </div>
       </div>
       <div
         css={css`
