14a6fb3969d0515fbdaf455d6ef38910ead9e499
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-07 22:04:46 +0900

    apollo client in source code

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "../styles/prism-vsc-dark-plus.css";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../libs/apolloClient";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
