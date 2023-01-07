94c4bd7fa094df9ea6e1a4bf13ebc5c9d0d90c94
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-07 16:49:16 +0900

    replace IDE inner with sourcecode/file-content

--- next/components/IDE.tsx
+++ next/components/IDE.tsx
@@ -5,6 +5,8 @@ import { FileNameComponent } from "./IDE/filetree/FileNameComponent";
 
 import Prism from "prismjs";
 import "prismjs/components/prism-protobuf"; //ts 7016 error suppressed by prism-fix.d.ts
+import { FileNameTabBar } from "./sourcecode/file-content/FileNameTabBar";
+import { FileContentViewer } from "./sourcecode/file-content/FileContentViewer";
 // prism stylesheet /styles/prism-xxx.css is imported from /pages/_app.tsx, as global stylesheet import is only allowed there.
 // https://nextjs.org/docs/messages/css-global
 
@@ -145,39 +147,12 @@ export const IDE = (): JSX.Element => {
           max-width: 520px; //necessary for wider-than-width source code
         `}
       >
-        <div
-          css={css`
-            background-color: #121212;
-          `}
-        >
-          <IDEEditorTab filename="package.json" />
-        </div>
-        <div
-          css={css`
-            height: ${sourceCodeHeight}px;
-            overflow: scroll; //scroll here, not to include file name tabe in the vertical scroll
-            ::-webkit-scrollbar {
-              width: 5px;
-              height: 5px;
-              background-color: #252526; /* or add it to the track */
-            }
-            ::-webkit-scrollbar-thumb {
-              background: #a0a0a0;
-              border-radius: 5px;
-            }
-          `}
-        >
-          <pre
-            css={css`
-              width: fit-content;
-              min-height: 100%; //expand up to the outer element
-            `}
-          >
-            <code className="language-protobuf" ref={ref}>
-              {sourceCode}
-            </code>
-          </pre>
-        </div>
+        <FileNameTabBar filename="package.json" />
+        <FileContentViewer
+          fileContent={sourceCode}
+          prismLanguage="protobuf"
+          sourceCodeHeight={sourceCodeHeight}
+        />
       </div>
     </div>
   );
