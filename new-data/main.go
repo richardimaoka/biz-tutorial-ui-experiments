5e60e6368790703a574ce44d08900a1e97cbd32d
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-11 08:45:02 +0900

    validating action

--- new-data/main.go
+++ new-data/main.go
@@ -4,7 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"reflect"
 )
+
+type Action interface {
+	IsAction()
+}
 
 type Command struct {
 	TypeName string `json:"__typename"`
@@ -35,10 +40,6 @@ type Terminal struct {
 	elements []interface{}
 }
 
-type Action interface {
-	IsAction()
-}
-
 type ActionInfo struct {
 	Action  Action
 	Results interface{}
@@ -50,29 +51,37 @@ type State struct {
 }
 
 func main() {
-	bytes, err := os.ReadFile("action01.json")
+	filename := "step01/action.json"
+	bytes, err := os.ReadFile(filename)
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("Error in reading action from file: %s", err))
 	}
 
 	var data interface{}
 	if err := json.Unmarshal(bytes, &data); err != nil {
-		panic(err)
+		panic(fmt.Sprintf("Error in unmarshaling JSON from file: %s is not a valid JSON, %s", filename, err))
 	}
 
-	asserted, ok := data.(map[string]interface{})
+	asserted, ok := data.(map[string]interface{}) //type assertion
 	if !ok {
-		panic("type assertion failed")
+		panic(fmt.Sprintf("Error in constructing Go data from JSON: Perhaps %s is not in JSON 'object'", filename))
 	}
 
-	action, ok := asserted["action"]
+	action, ok := asserted["__typename"]
 	if !ok {
-		panic("action does not exist")
+		panic(fmt.Sprintf("Error in validating action type: \"__typename\" does not exist in %s", filename))
 	}
 
-	switch v := action.(type) {
-		case Command
+	switch s := action.(type) {
+	case string:
+		switch s {
+		case "Command":
+			fmt.Printf("successfully retrieved action = %s from %s", s, filename)
+		default:
+			panic(fmt.Sprintf("Error in validating action type: %s is not a valid action type", s))
+		}
+	default:
+		panic(fmt.Sprintf("Error in validating action type: \"__typename\" = %v is in wrong type %v", s, reflect.TypeOf(s)))
 	}
 
-	fmt.Println(actions)
 }
