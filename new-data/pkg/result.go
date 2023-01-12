d6495cf410dcfad8dd638043f4eeafe0c2bf1272
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-13 07:13:42 +0900

    results

--- new-data/pkg/result.go
+++ new-data/pkg/result.go
@@ -27,7 +27,7 @@ type ChangeCurrentDirectoryInTerminal struct {
 func (c *ChangeCurrentDirectoryInTerminal) IsResult() {}
 
 func GetResultFromFile(filename string) (Result, error) {
-	errorPreceding := "Error in getResultFromFile for filename = " + filename
+	errorPreceding := "Error in GetResultFromFile for filename = " + filename
 
 	bytes, err := os.ReadFile(filename)
 	if err != nil {
@@ -82,4 +82,42 @@ func GetResultFromBytes(bytes []byte) (Result, error) {
 	default:
 		return nil, fmt.Errorf("\"__typename\" = %v is in wrong type %v", t, reflect.TypeOf(t))
 	}
+}
+
+func GetResultSliceFromFile(filename string) ([]Result, error) {
+	errorPreceding := "Error in GetResultSliceFromFile for filename = " + filename
+
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("%s, %s", errorPreceding, err)
+	}
+
+	resultList, err := GetResultSliceFromBytes(bytes)
+	if err != nil {
+		return nil, fmt.Errorf("%s, %s", errorPreceding, err)
+	}
+
+	return resultList, nil
+}
+
+func GetResultSliceFromBytes(bytes []byte) ([]Result, error) {
+	var unmarshaled []map[string]interface{}
+	if err := json.Unmarshal(bytes, &unmarshaled); err != nil {
+		return nil, err
+	}
+
+	var results []Result
+	for i, r := range unmarshaled {
+		resultBytes, err := json.Marshal(r)
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal back %s result %s", ordinal(i), err)
+		}
+		result, err := GetResultFromBytes(resultBytes)
+		if err != nil {
+			return nil, fmt.Errorf("failed construct %s result, %s", ordinal(i), err)
+		}
+		results = append(results, result)
+	}
+
+	return results, nil
 }
