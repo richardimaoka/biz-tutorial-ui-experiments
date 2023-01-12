d24aaa69c5789e154fa7a459ae5efe656cb07ac3
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-01-13 06:27:43 +0900

    update model

--- new-data/pkg/model.go
+++ new-data/pkg/model.go
@@ -20,3 +20,14 @@ type ActionInfo struct {
 	Action  Action
 	Results []Result
 }
+
+type FileTreeNode struct {
+	TypeName            string `json:"__typename"`
+	FilePath            []string
+	Offset              int
+	CurrentDirTerminals []string
+}
+
+type SourceCode struct {
+	FileTree []FileTreeNode
+}
