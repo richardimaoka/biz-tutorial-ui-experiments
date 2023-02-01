5026b1612d555fdb1bbb6ad69c2c1f521c647edc
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-02-02 03:48:44 +0900

    result and expected

--- new-data/model/unflatten_test.go
+++ new-data/model/unflatten_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestUnflatten(t *testing.T) {
 	result := unflatten([]byte(`{"parent.childA": "AAA", "parent.childB": 10, "a": 250}`))
-	expected := map[string]interface{}{"parent.childA": "AAA", "parent.childB": 10}
+	expected := map[string]interface{}{"parent": map[string]interface{}{"childA": "AAA", "childB": 10}, "a": 250}
 
 	for key, value := range result {
 		switch t := value.(type) {
@@ -16,17 +16,18 @@ func TestUnflatten(t *testing.T) {
 				fmt.Println("result  : key =", key, ", childKey =", childKey, ", childValue = ", childValue)
 			}
 		default:
-			fmt.Println("result  : key = ", key, "value = ", value)
+			fmt.Println("result  : key =", key, "value = ", value)
 		}
 	}
+
 	for key, value := range expected {
-		fmt.Println("expected: key =", key, ", value =", value)
+		switch t := value.(type) {
+		case map[string]interface{}:
+			for childKey, childValue := range t {
+				fmt.Println("expected: key =", key, ", childKey =", childKey, ", childValue = ", childValue)
+			}
+		default:
+			fmt.Println("expected: key =", key, "value = ", value)
+		}
 	}
-	// if  == expected["parent"]["childA"] {
-	// }
-	// for key, v := range expected {
-	// 	if v != expected[key] {
-	// 		t.Errorf("expected %q but got %q", expected, result)
-	// 	}
-	// }
 }
