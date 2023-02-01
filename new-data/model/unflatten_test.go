fa4660cf6a8ff1b167be4a7a7abdde51d2c75f6c
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-02-02 06:01:21 +0900

    updated test

--- new-data/model/unflatten_test.go
+++ new-data/model/unflatten_test.go
@@ -20,7 +20,7 @@ func compareTwoValues(t *testing.T, v1name string, v1 interface{}, v2name string
 	v1type := reflect.TypeOf(v1)
 	v2type := reflect.TypeOf(v2)
 	if v1type != v2type {
-		t.Errorf("%s has type = %v, not matching with %s in type = %v", v1name, v1type, v2name, v2type)
+		t.Errorf("%s = %v has type = %v, not matching with %s = %v having type = %v", v1name, v1, v1type, v2name, v2, v2type)
 		return
 	}
 
@@ -30,36 +30,46 @@ func compareTwoValues(t *testing.T, v1name string, v1 interface{}, v2name string
 	}
 }
 
+func compareTwoMaps(t *testing.T, m1name string, m1 interface{}, m2name string, m2 interface{}) {
+	m1Map, ok := m1.(map[string]interface{})
+	if !ok {
+		t.Errorf("%s has type = %v, not map[string]interface{}", reflect.TypeOf(m1), m1name)
+	}
+
+	m2Map, ok := m2.(map[string]interface{})
+	if !ok {
+		t.Errorf("%s has type = %v, not map[string]interface{}", reflect.TypeOf(m2), m2name)
+	}
+
+	for k, v1 := range m1Map {
+		v2, ok := m2Map[k]
+		if !ok {
+			t.Errorf("%s[%s] does not exist, while %s[%s] does", m2name, k, m1name, k)
+			return
+		}
+
+		compareTwoValues(t, fmt.Sprintf("%s[%s]", m1name, k), v1, fmt.Sprintf("%s[%s]", m2name, k), v2)
+	}
+
+	for k, v2 := range m2Map {
+		v1, ok := m1Map[k]
+		if !ok {
+			t.Errorf("%s[%s] does not exist, while %s[%s] does", m1name, k, m2name, k)
+			return
+		}
+
+		compareTwoValues(t, fmt.Sprintf("%s[%s]", m1name, k), v1, fmt.Sprintf("%s[%s]", m2name, k), v2)
+	}
+}
+
 func TestUnflatten(t *testing.T) {
-	result := unflatten([]byte(`{"parent.childA": "AAA", "parent.childB": 10, "a": 250, "b": "bbb"}`))
-	expected := map[string]interface{}{"parent": map[string]interface{}{"childA": "AAA", "childB": 10.0}, "a": 250, "c": 2520}
+	result := unflatten([]byte(`{"parent.childA": "AAA", "parent.childB": 10, "parent.childD": null, "a": 250, "b": "bbb", "d": 1234}`))
+	expected := map[string]interface{}{"parent": map[string]interface{}{"childA": "AAA", "childB": 10.0, "childC": nil}, "a": 250, "c": 2520}
 
 	for key, value := range result {
 		switch children := value.(type) {
 		case map[string]interface{}:
-			for childKey, childValue := range children {
-				fmt.Println("result  : key =", key, ", childKey =", childKey, ", childValue = ", childValue)
-
-				eChildren, ok := expected[key]
-				if !ok {
-					t.Errorf("expected[%s] does not exist, while result[%s] does", key, key)
-					return
-				}
-
-				ecMap, ok := eChildren.(map[string]interface{})
-				if !ok {
-					t.Errorf("expected[%s] is not a map[string]interface{}, while result[%s] is", key, key)
-					return
-				}
-
-				eChildValue, ok := ecMap[childKey]
-				if !ok {
-					t.Errorf("expected[%s][%s] does not exist, while result[%s][%s] does", key, childKey, key, childKey)
-					return
-				}
-
-				compareTwoValues(t, fmt.Sprintf("result[%s][%s]", key, childKey), childValue, fmt.Sprintf("expected[%s][%s]", key, childKey), eChildValue)
-			}
+			compareTwoMaps(t, fmt.Sprintf("result[%s]", key), children, fmt.Sprintf("expected[%s]", key), expected[key])
 		default:
 			eValue, ok := expected[key]
 			if !ok {
@@ -68,17 +78,6 @@ func TestUnflatten(t *testing.T) {
 			}
 
 			compareTwoValues(t, fmt.Sprintf("result[%s]", key), value, fmt.Sprintf("expected[%s]", key), eValue)
-		}
-	}
-
-	for key, value := range expected {
-		switch t := value.(type) {
-		case map[string]interface{}:
-			for childKey, childValue := range t {
-				fmt.Println("expected: key =", key, ", childKey =", childKey, ", childValue = ", childValue)
-			}
-		default:
-			fmt.Println("expected: key =", key, "value = ", value)
 		}
 	}
 }
