a84ffcd4dd94426a02b290939b921c9316880b5b
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-02-03 05:47:12 +0900

    unflatten real logic

--- new-data/model/unflatten.go
+++ new-data/model/unflatten.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"encoding/json"
+	"fmt"
+	"reflect"
 	"strings"
 )
 
@@ -15,32 +17,68 @@ func unflatten(bytes []byte) (map[string]interface{}, error) {
 	return unflattenMap(unmarshaled)
 }
 
-func unflattenMap(m map[string]interface{}) (map[string]interface{}, error) {
-	var nested = make(map[string]interface{})
-	for k, v := range m {
-		if dotIndex := strings.IndexRune(k, '.'); dotIndex == -1 {
-			// no nesting
-			nested[k] = v
-		} else {
-			// nesting needed
-			parentKey := k[0:dotIndex]
-			childKey := k[dotIndex+1:]
-
-			if nested[parentKey] == nil {
-				// if same parentKey not used
-				children := make(map[string]interface{})
-				children[childKey] = v
-				nested[parentKey] = children
-			} else {
-				// if same paretKey already found
-				children, ok := nested[parentKey].(map[string]interface{})
-				if !ok {
-					panic(parentKey + " conflicted!!")
-				}
-				children[childKey] = v
-			}
-		}
+func set(m map[string]interface{}, originalKey, key string, v interface{}) error {
+	//pre-condition check
+	existingValue, already := m[key]
+	if already {
+		return fmt.Errorf("key = %s (%s) alrady has value = %v, so failed to set value = %v", originalKey, key, existingValue, v)
+	}
+	if v == nil {
+		m[key] = nil
+		return nil
+	}
+	t := reflect.TypeOf(v)
+	switch t.Kind() {
+	case reflect.Slice:
+		return fmt.Errorf("key = %s (%s) has value = %v of type = %v, but slice value can't be handled", originalKey, key, v, t)
+	case reflect.Array:
+		return fmt.Errorf("key = %s (%s) has value = %v of type = %v, but array value can't be handled", originalKey, key, v, t)
+	case reflect.Map:
+		return fmt.Errorf("key = %s (%s) has value = %v of type = %v, but map value can't be handled", originalKey, key, v, t)
 	}
 
-	return nested, nil
+	// Pattern 1: if key doens't have '.', then simply set value and return
+	var dotIndex = strings.IndexRune(key, '.')
+	if dotIndex == -1 {
+		m[key] = v
+		return nil
+	}
+
+	// Pattern 2: if key has '.', then make children
+	parentKey := key[0:dotIndex]
+	childKey := key[dotIndex+1:]
+
+	_, already = m[parentKey]
+	if already {
+		// Pattern 2.1: parent key already used
+		mm, ok := m[parentKey].(map[string]interface{})
+		if !ok {
+			return fmt.Errorf("key = %s already has a non-map value at %s, which is supposedly dupe, so failed to set the value = %v", originalKey, key, v)
+		}
+		set(mm, originalKey, childKey, v)
+
+		return nil
+	} else {
+		// Pattern 2.2: parent key not used before
+		mm := make(map[string]interface{})
+		err := set(mm, originalKey, childKey, v)
+		if err != nil {
+			return err
+		}
+		m[parentKey] = mm
+
+		return nil
+	}
+}
+
+func unflattenMap(mo map[string]interface{}) (map[string]interface{}, error) {
+	m := make(map[string]interface{})
+
+	for k, v := range mo {
+		err := set(m, k, k, v)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return m, nil
 }
