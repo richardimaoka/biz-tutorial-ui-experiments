5adef1a8b236e46599651713a02acc331ed25d70
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-02-12 05:42:31 +0900

    SplitInputListFile

--- new-data/model/input.go
+++ new-data/model/input.go
@@ -28,6 +28,39 @@ func toUnflatJsonBytes(flatJsonObj map[string]interface{}) ([]byte, error) {
 	return unflatJsonBytes, nil
 }
 
+func SplitInputListFile(targetDir string) error {
+	actionListFile := targetDir + "action_input_list.json"
+	targetFilePrefix := "flat_input"
+
+	errorPreceding := "Error in SplitInputListFile for filename = " + actionListFile
+
+	// read and process the whole file
+	bytes, err := os.ReadFile(actionListFile)
+	if err != nil {
+		return fmt.Errorf("%s, reading action failed, %s", errorPreceding, err)
+	}
+
+	var unmarshalled []map[string]interface{}
+	if err := json.Unmarshal(bytes, &unmarshalled); err != nil {
+		return fmt.Errorf("%s, unmarshaling action failed, %s", errorPreceding, err)
+	}
+
+	// write each array element into file
+	for i, flatJsonObj := range unmarshalled {
+		jsonBytes, err := json.MarshalIndent(flatJsonObj, "", "  ")
+		if err != nil {
+			return fmt.Errorf("%s, marshaling %s ActionCommand failed, %s", errorPreceding, ordinal(i), err)
+		}
+
+		err = os.WriteFile(fmt.Sprintf("%s/%s%03d.json", targetDir, targetFilePrefix, i), jsonBytes, 0644)
+		if err != nil {
+			return fmt.Errorf("%s, writing %s action failed, %s", errorPreceding, ordinal(i), err)
+		}
+	}
+
+	return nil
+}
+
 func SplitActionListFile(actionListFile, targetDir, targetFilePrefix string) error {
 	errorPreceding := "Error in SplitActionListFile for filename = " + actionListFile
 
