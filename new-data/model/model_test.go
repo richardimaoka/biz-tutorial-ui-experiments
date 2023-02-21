7af2dacdf1c3dc3c67b5f4295800750c8341c045
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-02-21 15:20:49 +0900

    utils_Test

--- new-data/model/model_test.go
+++ new-data/model/model_test.go
@@ -1,56 +1,8 @@
 package model
 
 import (
-	"encoding/json"
-	"os"
 	"testing"
-
-	"github.com/google/go-cmp/cmp"
 )
-
-func prettyString(m map[string]interface{}) string {
-	jsonString, err := json.MarshalIndent(m, "", "  ")
-	if err != nil {
-		panic(err)
-	}
-	return string(jsonString)
-}
-
-func compareJsonBytes(t *testing.T, expectedBytes, resultBytes []byte) {
-	var resultMap map[string]interface{}
-	err := json.Unmarshal(resultBytes, &resultMap)
-	if err != nil {
-		t.Errorf("failed to unmarshal result json")
-		return
-	}
-
-	var expectedMap map[string]interface{}
-	err = json.Unmarshal(expectedBytes, &expectedMap)
-	if err != nil {
-		t.Errorf("failed to unmarshal expected json")
-		return
-	}
-
-	if diff := cmp.Diff(expectedMap, resultMap); diff != "" {
-		t.Errorf("mismatch (-expected +result):\n%s", diff)
-	}
-}
-
-func compareAfterMarshal(t *testing.T, expectedJsonFile string, result interface{}) {
-	expectedBytes, err := os.ReadFile(expectedJsonFile)
-	if err != nil {
-		t.Errorf("failed to read %s", expectedJsonFile)
-		return
-	}
-
-	resultBytes, err := json.Marshal(result)
-	if err != nil {
-		t.Errorf("failed to marshal result")
-		return
-	}
-
-	compareJsonBytes(t, expectedBytes, resultBytes)
-}
 
 func Test_NewPageState(t *testing.T) {
 	result := NewPageState()
