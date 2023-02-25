72d699ee53c08b25f7d282d6487ce48b96e18726
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-02-26 07:24:36 +0900

    add dir 3

--- new-data/model2/source_code_test.go
+++ new-data/model2/source_code_test.go
@@ -64,3 +64,24 @@ func TestAddDirectory2(t *testing.T) {
 	// json should be same as initial state
 	compareAfterMarshal(t, "testdata/add-directory2.json", sc)
 }
+
+func TestAddDirectory3(t *testing.T) {
+	filePathPtrSlice2("hello/world/goodmorning")
+
+	sc := newSourceCode()
+	if err := sc.addDirectory("hello"); err != nil {
+		t.Error(err)
+		return
+	}
+	if err := sc.addDirectory("hello/world"); err != nil {
+		t.Error(err)
+		return
+	}
+	if err := sc.addDirectory("aloha"); err != nil {
+		t.Error(err)
+		return
+	}
+
+	// json should be same as initial state
+	compareAfterMarshal(t, "testdata/add-directory3.json", sc)
+}
