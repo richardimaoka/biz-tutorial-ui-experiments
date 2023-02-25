a43399f29eb57f4420d37011eb0b3d0cf3f9a1a8
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-02-26 05:56:52 +0900

    file_node

--- new-data/model2/file_node.go
+++ new-data/model2/file_node.go
@@ -9,3 +9,46 @@ func (f *FileNode) FilePathString() string {
 	}
 	return strings.Join(s, "/")
 }
+
+func filePathPtrSlice(filePath string) []*string {
+	split := strings.Split(filePath, "/")
+
+	var filePathSlice []*string
+	for _, v := range split {
+		filePathSlice = append(filePathSlice, &v)
+	}
+
+	return filePathSlice
+}
+
+func directoryNode(filePath string) *FileNode {
+	nodeType := FileNodeTypeDirectory
+	split := filePathPtrSlice(filePath)
+	offset := len(split) - 1
+	trueValue := true
+
+	node := FileNode{
+		NodeType:  &nodeType,
+		Name:      split[len(split)-1],
+		FilePath:  split,
+		Offset:    &offset,
+		IsUpdated: &trueValue,
+	}
+	return &node
+}
+
+func fileNode(filePath string) *FileNode {
+	nodeType := FileNodeTypeFile
+	split := filePathPtrSlice(filePath)
+	offset := len(split) - 1
+	trueValue := true
+
+	node := FileNode{
+		NodeType:  &nodeType,
+		Name:      split[len(split)-1],
+		FilePath:  split,
+		Offset:    &offset,
+		IsUpdated: &trueValue,
+	}
+	return &node
+}
