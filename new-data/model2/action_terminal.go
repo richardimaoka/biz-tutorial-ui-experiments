4b0bb4eedabd5525766c4b3dca9214412e2f1793
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-02-26 23:39:13 +0900

    move around methods

--- new-data/model2/action_terminal.go
+++ new-data/model2/action_terminal.go
@@ -1,79 +1,8 @@
 package model2
 
-import (
-	"fmt"
-	"reflect"
-)
-
 type ActionTerminal struct {
 	Command          string
 	TerminalName     string
 	Output           string //if "", no output after execution
 	CurrentDirectory string //if "", current directory is not changed after execution
-}
-
-//no pre-condition required, always succeed
-func (t *Terminal) typeIn(action ActionTerminal) {
-	trueValue := true
-	node := TerminalNode{
-		Content: TerminalCommand{
-			Command:         &action.Command,
-			BeforeExecution: &trueValue,
-		},
-	}
-
-	// works even if Nodes is nil
-	t.Nodes = append(t.Nodes, &node)
-}
-
-//no pre-condition required, always succeed
-func (t *Terminal) changeDirectory(action ActionTerminal) {
-	t.CurrentDirectoryPath = &action.CurrentDirectory
-}
-
-//no pre-condition required, always succeed
-func (t *Terminal) writeOutput(action ActionTerminal) {
-	node := TerminalNode{
-		Content: TerminalOutput{
-			Output: &action.Output,
-		},
-	}
-
-	// works even if Nodes is nil
-	t.Nodes = append(t.Nodes, &node)
-}
-
-// pre-condition check = isLastCommandExecutable()
-func (t *Terminal) markLastCommandExecuted() error {
-	lastNode, err := t.getLastNode()
-	if err != nil {
-		return fmt.Errorf("%s", err)
-	}
-
-	lastCommand, ok := lastNode.Content.(TerminalCommand)
-	if !ok {
-		return fmt.Errorf("terminal's last node is not TerminalCommand but %v", reflect.TypeOf(lastNode.Content))
-	}
-
-	lastNode.Content = lastCommand.toExecutedCommand()
-	return nil
-}
-
-// assuming typeIn() is called earlier
-func (t *Terminal) Execute(action ActionTerminal) error {
-	//action.validate()
-	//t.validate(action)
-	if err := t.markLastCommandExecuted(); err != nil {
-		return err
-	}
-
-	if action.Output != "" {
-		t.writeOutput(action)
-	}
-
-	if action.CurrentDirectory != "" {
-		t.changeDirectory(action)
-	}
-
-	return nil
 }
--- new-data/model2/terminal.go
+++ new-data/model2/terminal.go
@@ -2,12 +2,23 @@ package model2
 
 import (
 	"fmt"
+	"reflect"
 )
 
-func newTerminal(name string) *Terminal {
-	return &Terminal{
-		Name: &name,
+// pre-condition check = isLastCommandExecutable()
+func (t *Terminal) markLastCommandExecuted() error {
+	lastNode, err := t.getLastNode()
+	if err != nil {
+		return fmt.Errorf("%s", err)
 	}
+
+	lastCommand, ok := lastNode.Content.(TerminalCommand)
+	if !ok {
+		return fmt.Errorf("terminal's last node is not TerminalCommand but %v", reflect.TypeOf(lastNode.Content))
+	}
+
+	lastNode.Content = lastCommand.toExecutedCommand()
+	return nil
 }
 
 func (t *Terminal) getLastNode() (*TerminalNode, error) {
@@ -21,4 +32,80 @@ func (t *Terminal) getLastNode() (*TerminalNode, error) {
 	}
 
 	return lastNode, nil
+}
+
+func (t *Terminal) isLastCommandExecutable() error {
+	lastNode, err := t.getLastNode()
+	if err != nil {
+		return fmt.Errorf("failed to terminal's last node, %s", err)
+	}
+
+	cmd, ok := lastNode.Content.(TerminalCommand)
+	if !ok {
+		return fmt.Errorf("terminal's last node is not TerminalCommand but %v", reflect.TypeOf(lastNode.Content))
+	}
+
+	if cmd.BeforeExecution == nil || *cmd.BeforeExecution == false {
+		return fmt.Errorf("terminal's last command is not ready for execution")
+	}
+
+	return nil
+}
+
+// public methods
+
+func NewTerminal(name string) *Terminal {
+	return &Terminal{
+		Name: &name,
+	}
+}
+
+//no pre-condition required, always succeed
+func (t *Terminal) TypeInCommand(command string) {
+	trueValue := true
+	node := TerminalNode{
+		Content: TerminalCommand{
+			Command:         &command,
+			BeforeExecution: &trueValue,
+		},
+	}
+
+	// works even if Nodes is nil
+	t.Nodes = append(t.Nodes, &node)
+}
+
+//no pre-condition required, always succeed
+func (t *Terminal) ChangeCurrentDirectory(filePath string) {
+	t.CurrentDirectoryPath = &filePath
+}
+
+//no pre-condition required, always succeed
+func (t *Terminal) writeOutput(output string) {
+	node := TerminalNode{
+		Content: TerminalOutput{
+			Output: &output,
+		},
+	}
+
+	// works even if Nodes is nil
+	t.Nodes = append(t.Nodes, &node)
+}
+
+// assuming TypeInCommand() is called earlier
+func (t *Terminal) Execute(action ActionTerminal) error {
+	//action.validate()
+	//t.validate(action)
+	if err := t.markLastCommandExecuted(); err != nil {
+		return err
+	}
+
+	if action.Output != "" {
+		t.writeOutput(action.Output)
+	}
+
+	if action.CurrentDirectory != "" {
+		t.ChangeCurrentDirectory(action.CurrentDirectory)
+	}
+
+	return nil
 }
