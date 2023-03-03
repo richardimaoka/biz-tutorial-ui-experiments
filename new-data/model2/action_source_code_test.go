302eba011e32906791792b7708741ba0b8109c39
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-03-04 00:16:35 +0900

    file add test case

--- new-data/model2/action_source_code_test.go
+++ new-data/model2/action_source_code_test.go
@@ -7,8 +7,13 @@ import (
 func TestActionSourceCodeAddFile1(t *testing.T) {
 	add := FileAdd{FilePath: "hello.txt"}
 	sourceCode := NewSourceCode()
-	sourceCode.AddFileNode(add.FilePath)
-	sourceCode.AddFileContent(add.FilePath, add.Content, add.IsFullContent)
+	if err := sourceCode.AddFileNode(add.FilePath); err != nil {
+		t.Fatal(err)
+	}
+	if err := sourceCode.AddFileContent(add.FilePath, add.Content, add.IsFullContent); err != nil {
+		t.Fatal(err)
+	}
+	compareAfterMarshal(t, "testdata/action/command/file-add1.json", sourceCode)
 }
 
 //TODO: directry translate AddFile, UpdateFile, ... etc to SourceCode by invoking SourceCode's methods
