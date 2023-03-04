7b8b9a3af245cda399335925657bfff939dc8403
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-03-04 23:14:25 +0900

    avoid unnecessary *[]string

--- new-data/model2/file_path.go
+++ new-data/model2/file_path.go
@@ -5,35 +5,25 @@ import (
 	"strings"
 )
 
-//TODO: actually no need to convert to ptrs
-func toStrPtrArray(arr []string) []*string {
-	var ptrSlice []*string
-	for i := range arr {
-		ptrSlice = append(ptrSlice, &arr[i]) // cannot use v of `for i, v := range ...` because v has the same address throughout the loop
-	}
-
-	return ptrSlice
-}
-
 func LessFilePath(a, b string) bool {
-	splitA := toStrPtrArray(strings.Split(a, "/"))
-	splitB := toStrPtrArray(strings.Split(b, "/"))
+	splitA := strings.Split(a, "/")
+	splitB := strings.Split(b, "/")
 	return lessFilePathInner(splitA, splitB)
 }
 
 // for debugging
-func printStrPtrArray(a, b []*string) {
-	for _, v := range a {
-		fmt.Print(*v, ", ")
-	}
-	fmt.Print("vs. ")
-	for _, v := range b {
-		fmt.Print(*v, ", ")
-	}
-	fmt.Println()
-}
+// func printStrPtrArray(a, b []*string) {
+// 	for _, v := range a {
+// 		fmt.Print(*v, ", ")
+// 	}
+// 	fmt.Print("vs. ")
+// 	for _, v := range b {
+// 		fmt.Print(*v, ", ")
+// 	}
+// 	fmt.Println()
+// }
 
-func lessFilePathInner(a, b []*string) bool {
+func lessFilePathInner(a, b []string) bool {
 	if len(a) == 0 && len(b) == 0 {
 		// a == b. even if len(b) == 0
 		return false
@@ -47,10 +37,10 @@ func lessFilePathInner(a, b []*string) bool {
 
 	// now len(a) != 0 AND len(b) != 0
 
-	if *a[0] == *b[0] {
+	if a[0] == b[0] {
 		return lessFilePathInner(a[1:], b[1:])
 	} else {
-		return *a[0] < *b[0]
+		return a[0] < b[0]
 	}
 }
 
