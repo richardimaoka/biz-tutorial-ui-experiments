ea720b593e6be1e2211c1a3b8e939f8e9ec5d5c7
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-03-04 18:39:15 +0900

    file add 2

--- new-data/model2/action_source_code_test.go
+++ new-data/model2/action_source_code_test.go
@@ -1,6 +1,7 @@
 package model2
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -14,6 +15,23 @@ func TestActionSourceCodeAddFile1(t *testing.T) {
 		t.Fatal(err)
 	}
 	compareAfterMarshal(t, "testdata/action/command/file-add1.json", sourceCode)
+}
+
+func TestActionSourceCodeAddFile2(t *testing.T) {
+	add := FileAdd{FilePath: "hello/world.txt", Content: "Hello World", IsFullContent: true}
+	sourceCode := NewSourceCode()
+	split := strings.Split(add.FilePath, "/")
+	helloDir := split[0]
+	if err := sourceCode.AddDirectoryNode(helloDir); err != nil {
+		t.Fatal(err)
+	}
+	if err := sourceCode.AddFileNode(add.FilePath); err != nil {
+		t.Fatal(err)
+	}
+	if err := sourceCode.AddFileContent(add.FilePath, add.Content, add.IsFullContent); err != nil {
+		t.Fatal(err)
+	}
+	compareAfterMarshal(t, "testdata/action/command/file-add2.json", sourceCode)
 }
 
 //TODO: directry translate AddFile, UpdateFile, ... etc to SourceCode by invoking SourceCode's methods
