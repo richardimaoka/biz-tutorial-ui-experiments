fdce12f457f2346760f6642f9d9f0cdb503c1f9d
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-04-08 18:24:08 +0900

    it's now main.go instead of server.go

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -9,7 +9,7 @@ tmux send-keys -t "$SESSION:0.0" '(cd gqlgen && gow -e=gql run github.com/99desi
 tmux split-window -v -t "$SESSION"
 tmux select-layout even-vertical   # to avoid 'no space for new pane' 
 
-tmux send-keys -t "$SESSION:0.1" '(cd gqlgen && gow -e=go,json run server.go)' C-m
+tmux send-keys -t "$SESSION:0.1" '(cd gqlgen && gow -e=go,json run main.go)' C-m
 tmux split-window -v -t "$SESSION"
 tmux select-layout even-vertical   # to avoid 'no space for new pane' 
 
