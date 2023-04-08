490340e2cbc0e14b48d255165e2e780a8ed773e3
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-04-09 08:05:23 +0900

    remove openFile

--- gqlgen/graph/model/models_manual.go
+++ gqlgen/graph/model/models_manual.go
@@ -2,6 +2,5 @@ package model
 
 type SourceCode struct {
 	FileTree     []*FileNode         `json:"fileTree"`
-	OpenFile     *OpenFile           `json:"openFile"`
 	FileContents map[string]OpenFile `json:"fileContents"`
 }
