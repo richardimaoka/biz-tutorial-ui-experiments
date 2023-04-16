e82d7fa64a85384f882f6eb23ab39fcbad8d6640
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-04-17 00:07:24 +0900

    with the current json-based server, best to put step in SourceCode

--- gqlgen/graph/model/models_manual.go
+++ gqlgen/graph/model/models_manual.go
@@ -1,6 +1,7 @@
 package model
 
 type SourceCode struct {
+	Step            *string             `json:"step"` //This is not exposed in the schema, so that SourceCode needs to be a manual type
 	DefaultOpenFile *OpenFile           `json:"defaultOpenFile"`
 	FileTree        []*FileNode         `json:"fileTree"`
 	FileContents    map[string]OpenFile `json:"fileContents"` //This is not exposed in the schema, so that SourceCode needs to be a manual type
