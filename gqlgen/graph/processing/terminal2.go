7fd8fbbf6ac5c31139f3831879abedd2c6761884
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-04-23 02:53:19 +0900

    move to terminal_element.go

--- gqlgen/graph/processing/terminal2.go
+++ gqlgen/graph/processing/terminal2.go
@@ -4,44 +4,6 @@ import (
 	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/graph/model"
 )
 
-type terminalElement interface {
-	String() string
-	ToTerminalElement() model.TerminalElement
-}
-
-type terminalCommand struct {
-	promptExpression string
-	promptSymbol     string
-	command          string
-}
-
-type terminalOutput struct {
-	output string
-}
-
-func (t *terminalCommand) String() string {
-	//TODO: reflect promptExpression and promptSymbol
-	return t.command
-}
-
-func (t *terminalOutput) String() string {
-	return t.output
-}
-
-func (t *terminalCommand) ToTerminalElement() model.TerminalElement {
-	falseValue := false
-	return &model.TerminalCommand{
-		BeforeExecution: &falseValue,
-		Command:         &t.command,
-	}
-}
-
-func (t *terminalOutput) ToTerminalElement() model.TerminalElement {
-	return &model.TerminalOutput{
-		Output: &t.output,
-	}
-}
-
 type Terminal2 struct {
 	terminalName     string
 	currentDirectory string
