a01cf2e51048ce968da174aa61ede9901fae0413
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-04-26 08:03:04 +0900

    test updated

--- gqlgen/graph/processing/file_node_test.go
+++ gqlgen/graph/processing/file_node_test.go
@@ -26,9 +26,23 @@ func TestFileNode_MutateFile(t *testing.T) {
 	compareAfterMarshal(t, "testdata/source_code/file-node-mutation2.json", result)
 
 	// after SourceCode is materialized to GraphQL object, mutation should have no effect
-	processor.fileMap["hello/world"].(*fileProcessorNode).isUpdated = true
+	processor.fileMap["hello/world"].(*fileProcessorNode).isUpdated = false
 	processor.fileMap["hello/world"].(*fileProcessorNode).filePath = "something/else"
 	processor.fileMap["hello/world"].(*fileProcessorNode).content = "contenttttt ttt tt"
 
 	compareAfterMarshal(t, "testdata/source_code/file-node-mutation2.json", result)
+}
+
+func TestFileNodeOpenFile_MutateFile(t *testing.T) {
+	processor := NewSourceCodeProcessor()
+	processor.AddFile(model.FileAdd{FilePath: "hello/world"})
+	result := processor.fileMap["hello/world"].(*fileProcessorNode).ToGraphQLOpenFile()
+	compareAfterMarshal(t, "testdata/source_code/file-node-mutation-openfile1.json", result)
+
+	// after SourceCode is materialized to GraphQL object, mutation should have no effect
+	processor.fileMap["hello/world"].(*fileProcessorNode).isUpdated = false
+	processor.fileMap["hello/world"].(*fileProcessorNode).filePath = "something/else"
+	processor.fileMap["hello/world"].(*fileProcessorNode).content = "contenttttt ttt tt"
+
+	compareAfterMarshal(t, "testdata/source_code/file-node-mutation-openfile1.json", result)
 }
