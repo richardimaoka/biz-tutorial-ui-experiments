52175fc8c9dd34f493a7c44a66a5317a1d179005
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-04-26 04:59:40 +0900

    move terminal element test to a dedicated _test filegg

--- gqlgen/graph/processing/terminal_processor_test.go
+++ gqlgen/graph/processing/terminal_processor_test.go
@@ -64,30 +64,3 @@ func TestTerminal_Clone(t *testing.T) {
 	result := terminalOriginal.ToTerminal()
 	compareAfterMarshal(t, "testdata/terminal/clone.json", result)
 }
-
-func TestTerminal_MutationCommand(t *testing.T) {
-	terminal := NewTerminalProcessor("default")
-	terminal.WriteCommand("mkdir abc")
-	terminal.WriteCommand("mkdir def")
-	result := terminal.ToTerminal()
-
-	// after terminal is materialized to GraphQL object, mutation should have no effect
-	terminal.elements[0].(*terminalCommand).promptExpression = "mutated-command"
-	terminal.elements[0].(*terminalCommand).promptSymbol = 'G'
-	terminal.elements[0].(*terminalCommand).command = "mkdir ghi"
-
-	compareAfterMarshal(t, "testdata/terminal/write-command2.json", result)
-}
-
-func TestTerminal_MutationOutput(t *testing.T) {
-	terminal := NewTerminalProcessor("default")
-	terminal.WriteCommand("echo abc")
-	terminal.WriteOutput("abc")
-
-	result := terminal.ToTerminal()
-
-	// after terminal is materialized to GraphQL object, mutation should have no effect
-	terminal.elements[1].(*terminalOutput).output = "def"
-
-	compareAfterMarshal(t, "testdata/terminal/write-output1.json", result)
-}
