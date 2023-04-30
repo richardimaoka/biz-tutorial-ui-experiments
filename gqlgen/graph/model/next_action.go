f4dcbbb7021ed18893e2dd2a8854c9cdc013f2e8
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-04-30 15:06:25 +0900

    unmarshal next action

---
+++ gqlgen/graph/model/next_action.go
@@ -0,0 +1,55 @@
+package model
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func (t *NextAction) UnmarshalJSON(b []byte) error {
+	var unmarshald map[string]interface{}
+	err := json.Unmarshal(b, &unmarshald)
+	if err != nil {
+		return err
+	}
+
+	bytes, err := json.Marshal(unmarshald["content"])
+	if err != nil {
+		return err
+	}
+
+	content, err := nextActionContentFromBytes(bytes)
+	if err != nil {
+		return err
+	}
+	t.Content = content
+
+	return nil
+}
+
+func nextActionContentFromBytes(bytes []byte) (NextActionContent, error) {
+	fromField := "contentType"
+	typename, err := extractTypeName(bytes, fromField)
+	if err != nil {
+		return nil, err
+	}
+
+	switch typename {
+	case "ActionTerminal":
+		var action ActionTerminal
+		if err := json.Unmarshal(bytes, &action); err != nil {
+			return nil, err
+		}
+		return &action, nil
+
+	case "ActionManual":
+		var action ActionManual
+		if err := json.Unmarshal(bytes, &action); err != nil {
+			return nil, err
+		}
+
+		return &action, nil
+
+	default:
+		return nil, fmt.Errorf("\"%s\" = %s is not a valid NextActionContent type", fromField, typename)
+	}
+}
