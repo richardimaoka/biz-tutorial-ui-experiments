297df4c3a715583278e81b23e21d00170bd97076
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-02 08:46:28 +0900

    remove new-data as it has not been used for long

--- new-data/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/richardimaoka/biz-tutorial-ui-experiments/new-data
-
-go 1.18
-
-require github.com/google/go-cmp v0.5.9
--- new-data/model/model_manual.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type SourceCode struct {
-	FileTree     []*FileNode         `json:"fileTree"`
-	OpenFile     *OpenFile           `json:"openFile"`
-	FileContents map[string]OpenFile `json:"fileContents"`
-}
