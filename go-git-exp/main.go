6b2f354b930d8d947e3a54c383fba27d55041030
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-02 08:55:39 +0900

    manual update

---
+++ go-git-exp/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-git/go-git/v5"
+)
+
+func main() {
+	r, err := git.PlainClone("/tmp/foo", false, &git.CloneOptions{
+		URL:      "https://github.com/go-git/go-git",
+		Progress: os.Stdout,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("r", r)
+}
