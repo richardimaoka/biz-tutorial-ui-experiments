827c8e7116dfabb2449c981d4c700e75531b7a56
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-02 21:02:45 +0900

    basic info

--- go-git-exp/main.go
+++ go-git-exp/main.go
@@ -9,18 +9,42 @@ import (
 
 func main() {
 	var r *git.Repository
-	r, err := git.PlainOpen("/tmp/foxo")
+	r, err := git.PlainOpen("/tmp/foo")
 
 	if err != nil {
 		fmt.Println(err)
 
-		r, err = git.PlainClone("/tmp/foo2", false, &git.CloneOptions{
-			URL:      "https://github.com/go-git/go-git",
+		r, err = git.PlainClone("/tmp/foo", false, &git.CloneOptions{
+			URL:      "https://github.com/richardimaoka/gqlgensandbox",
 			Progress: os.Stdout,
 		})
 
 		if err != nil {
 			panic(err)
+		}
+	}
+
+	itr, err := r.CommitObjects()
+	for {
+		ref, err := itr.Next()
+		if err != nil {
+			break
+		}
+		fmt.Println(ref.Hash)
+		fmt.Println(ref.Author)
+		fmt.Println(ref.Committer)
+		fmt.Println(ref.Message)
+
+		fileItr, err := ref.Files()
+		if err != nil {
+			break
+		}
+		for {
+			file, err := fileItr.Next()
+			if err != nil {
+				break
+			}
+			fmt.Println(file.Name)
 		}
 	}
 
