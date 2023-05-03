d8974e7535a1923d6f177cc5e91f642f43254a69
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-04 03:42:55 +0900

    first test

--- gqlgen/graph/gitmodel/commit.go
+++ gqlgen/graph/gitmodel/commit.go
@@ -1,12 +1,37 @@
 package gitmodel
 
 import (
+	"log"
+
+	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/storage/memory"
 )
 
 type File struct {
-	path string
-	hash plumbing.Hash
+	path       string
+	commitHash plumbing.Hash
+	Size       int64
+}
+
+func NewFile(repoUrl string, blobHash plumbing.Hash) *File {
+	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		URL: repoUrl,
+	})
+	if err != nil {
+		log.Println("error cloning repo")
+		return nil
+	}
+
+	blob, err := r.BlobObject(blobHash)
+	if err != nil {
+		log.Println("error getting blog")
+		return nil
+	}
+
+	log.Println("size", blob.Size)
+
+	return &File{Size: blob.Size}
 }
 
 func (f *File) Contents() *string {
--- gqlgen/graph/gitmodel_test/commit_test.go
+++ gqlgen/graph/gitmodel_test/commit_test.go
@@ -3,11 +3,20 @@ package gitmodel_test
 import (
 	"testing"
 
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/graph/gitmodel"
 )
 
 func TestActionCommandMarshal(t *testing.T) {
-	f := &gitmodel.File{}
-	f.Contents()
-	t.Log(f.Contents())
+	f := gitmodel.NewFile(
+		"https://github.com/richardimaoka/gqlgensandbox",
+		plumbing.NewHash("18ebd6486e9d929f614aba39bd0a2f7bb074d34d"))
+
+	t.Log(f.Size)
+	t.Errorf("test")
+
+	if f.Size == 0 {
+		t.Errorf("size is 0")
+	}
+
 }
