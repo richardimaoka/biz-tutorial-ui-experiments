5eda5faa94b49648526780d0330a5378be4700d8
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-04 21:42:20 +0900

    test working except isUpdated

--- gqlgen/graph/gitmodel/file.go
+++ gqlgen/graph/gitmodel/file.go
@@ -2,41 +2,49 @@ package gitmodel
 
 import (
 	"io"
-	"log"
+	"strings"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/graph/model"
 )
 
 type FileFromGit struct {
-	filePath       string
-	commitHash     plumbing.Hash
-	blobHash       plumbing.Hash
-	repo           *git.Repository
-	Size           int64
-	_Contents      string
-	IsFullContents bool
+	filePath string
+	blob     *object.Blob
+	commit   *object.Commit
 }
 
-func NewFileFromGit(repo *git.Repository, filePath string, blobHash plumbing.Hash) (*FileFromGit, error) {
+func NewFileFromGit(repo *git.Repository, filePath string, blobHash, commitHash plumbing.Hash) (*FileFromGit, error) {
 	blob, err := repo.BlobObject(blobHash)
 	if err != nil {
 		return nil, err
 	}
 
-	log.Println("size", blob.Size)
+	commit, err := repo.CommitObject(commitHash)
+	if err != nil {
+		return nil, err
+	}
 
-	return &FileFromGit{Size: blob.Size, blobHash: blobHash, repo: repo, filePath: filePath}, nil
+	return &FileFromGit{
+		blob:     blob,
+		commit:   commit,
+		filePath: filePath}, nil
+}
+
+func (f *FileFromGit) offset() int {
+	split := strings.Split(f.filePath, "/")
+	return len(split) - 1
+}
+
+func (f *FileFromGit) name() string {
+	split := strings.Split(f.filePath, "/")
+	return split[len(split)-1]
 }
 
 func (f *FileFromGit) Contents() *string {
-	blob, err := f.repo.BlobObject(f.blobHash)
-	if err != nil {
-		return nil
-	}
-
-	reader, err := blob.Reader()
+	reader, err := f.blob.Reader()
 	if err != nil {
 		return nil
 	}
@@ -51,8 +59,15 @@ func (f *FileFromGit) Contents() *string {
 }
 
 func (f *FileFromGit) FileNode() *model.FileNode {
+	name := f.name()
+	nodeType := model.FileNodeTypeFile
+	offset := f.offset()
+
 	return &model.FileNode{
 		FilePath: &f.filePath, //pointer is safe here, as f.filePath is effectively immutable
+		Name:     &name,
+		NodeType: &nodeType,
+		Offset:   &offset,
 	}
 }
 
--- gqlgen/graph/gitmodel_test/file_test.go
+++ gqlgen/graph/gitmodel_test/file_test.go
@@ -20,6 +20,7 @@ func TestFilePath(t *testing.T) {
 
 	type TestCase struct {
 		FilePath        string
+		CommitHash      string
 		BlobHash        string
 		ExpectationFile string
 	}
@@ -27,10 +28,12 @@ func TestFilePath(t *testing.T) {
 	testCases := []TestCase{
 		{
 			FilePath:        "graph/resolver.go",
+			CommitHash:      "4dd8f51d6acbee9d61b24dc26715ecc48a5d2456",
 			BlobHash:        "a25c09c619f9ed0db2ef05ece3429624b22a0f59",
 			ExpectationFile: "testdata/resolver.go.json",
 		}, {
 			FilePath:        "server.go",
+			CommitHash:      "4dd8f51d6acbee9d61b24dc26715ecc48a5d2456",
 			BlobHash:        "c0f5b2c8f1bf0ebbf713cc213b378af90a14f061",
 			ExpectationFile: "testdata/server.go.json",
 		},
@@ -38,7 +41,12 @@ func TestFilePath(t *testing.T) {
 
 	for _, c := range testCases {
 		t.Run(c.FilePath, func(t *testing.T) {
-			f, err := gitmodel.NewFileFromGit(repo, c.FilePath, plumbing.NewHash(c.BlobHash))
+			f, err := gitmodel.NewFileFromGit(
+				repo,
+				c.FilePath,
+				plumbing.NewHash(c.BlobHash),
+				plumbing.NewHash(c.CommitHash))
+
 			if err != nil {
 				t.Fatalf("error creating file: %v", err)
 			}
