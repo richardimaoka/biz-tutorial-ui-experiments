2acbc84098c5569a1cf280222df540b7bfe585f3
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-07 14:53:09 +0900

    test simplified

--- gqlgen/graph/processing_test/terminal_processor_test.go
+++ gqlgen/graph/processing_test/terminal_processor_test.go
@@ -9,96 +9,63 @@ import (
 	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/graph/processing"
 )
 
-// TODO: rewrite tests with table driven tests
-// TODO: private-ise the methods exept Transition
-// TODO: move the test to processing_test
-//
-// type TerminalEffect struct {
-// 	SeqNo            int     `json:"seqNo"`
-// 	TerminalName     string  `json:"terminalName"`
-// 	Command          string  `json:"command"`
-// 	Output           *string `json:"output"`           //if zero value, no output after execution
-// 	CurrentDirectory *string `json:"currentDirectory"` //if zero value, current directory is not changed after execution
-// }
-// var cases := []TerminalEffect{
-// 	{ SeqNo: 0, TerminalName: "default", Command: "echo abc", Output: nil, CurrentDirectory: nil },
-// }
+func Test_NewTerminalProcessor(t *testing.T) {
+	terminal := processing.NewTerminalProcessor("default")
+	result := terminal.ToGraphQLTerminal()
+	internal.CompareAfterMarshal(t, "testdata/terminal/new-terminal.json", result)
+}
 
-// test case for TerminalProcessor's WriteCommand method
-
-func Test_Terminal(t *testing.T) {
+func Test_TerminalEffect(t *testing.T) {
 	type TestCase struct {
 		ExpectedFile   string
 		TerminalEffect processing.TerminalEffect
 	}
 
-	testCases := []TestCase{
-		{"testdata/terminal/terminal1-1.json", processing.TerminalEffect{SeqNo: 0, TerminalName: "default", Command: "mkdir abc", Output: nil, CurrentDirectory: nil}},
-		// {"testdata/terminal/terminal1-2.json", processing.TerminalEffect{SeqNo: 1, TerminalName: "default", Command: "mkdir abc", Output: nil, CurrentDirectory: internal.Address("def")}},
-		// {"testdata/terminal/terminal1-3.json", processing.TerminalEffect{SeqNo: 2, TerminalName: "default", Command: "echo abc", Output: internal.Address("abc"), CurrentDirectory: nil}},
-		// {"testdata/terminal/terminal1-4.json", processing.TerminalEffect{SeqNo: 3, TerminalName: "default", Command: "mkdir def", Output: nil, CurrentDirectory: nil}},
-		// {"testdata/terminal/terminal1-5.json", processing.TerminalEffect{SeqNo: 4, TerminalName: "default", Command: "mkdir abc", Output: nil, CurrentDirectory: nil}},
-	}
-
-	terminal := processing.NewTerminalProcessor("default")
-	for _, c := range testCases {
-		t.Run("tt.descriptio", func(t *testing.T) {
+	runTestCases := func(t *testing.T, testCases []TestCase) {
+		terminal := processing.NewTerminalProcessor("default")
+		for _, c := range testCases {
 			step := fmt.Sprintf("%03d", c.TerminalEffect.SeqNo)
 			terminal.Transition(c.TerminalEffect, step)
 
+			// if `--update` flag is passed from command line, update the golden file
 			if *update {
 				internal.WriteGoldenFile(t, c.ExpectedFile, terminal.ToGraphQLTerminal())
 			}
 
 			internal.CompareAfterMarshal(t, c.ExpectedFile, terminal.ToGraphQLTerminal())
-		})
+		}
 	}
-}
 
-func TestTerminal(t *testing.T) {
-	terminal := processing.NewTerminalProcessor("default")
+	t.Run("write_command1", func(t *testing.T) {
+		runTestCases(t, []TestCase{
+			{"testdata/terminal/terminal1-1.json", processing.TerminalEffect{SeqNo: 0, TerminalName: "default", Command: "mkdir abc", Output: nil, CurrentDirectory: nil}},
+		})
+	})
 
-	result := terminal.ToGraphQLTerminal()
-	internal.CompareAfterMarshal(t, "testdata/terminal/new-terminal.json", result)
-}
+	t.Run("write_command2", func(t *testing.T) {
+		runTestCases(t, []TestCase{
+			{"testdata/terminal/terminal2-1.json", processing.TerminalEffect{SeqNo: 0, TerminalName: "default", Command: "mkdir abc", Output: nil, CurrentDirectory: nil}},
+			{"testdata/terminal/terminal2-2.json", processing.TerminalEffect{SeqNo: 0, TerminalName: "default", Command: "mkdir def", Output: nil, CurrentDirectory: nil}},
+		})
+	})
 
-func TestTerminal_WriteCommand1(t *testing.T) {
-	terminal := processing.NewTerminalProcessor("default")
-	terminal.WriteCommand("mkdir abc")
-	result := terminal.ToGraphQLTerminal()
-	internal.CompareAfterMarshal(t, "testdata/terminal/write-command1.json", result)
-}
+	t.Run("write_output", func(t *testing.T) {
+		runTestCases(t, []TestCase{
+			{"testdata/terminal/terminal3-1.json", processing.TerminalEffect{SeqNo: 0, TerminalName: "default", Command: "echo abc", Output: internal.Address("abc"), CurrentDirectory: nil}},
+		})
+	})
 
-func TestTerminal_WriteCommand2(t *testing.T) {
-	terminal := processing.NewTerminalProcessor("default")
-	terminal.WriteCommand("mkdir abc")
-	terminal.WriteCommand("mkdir def")
-	result := terminal.ToGraphQLTerminal()
-	internal.CompareAfterMarshal(t, "testdata/terminal/write-command2.json", result)
-}
+	t.Run("change_current_directory1", func(t *testing.T) {
+		runTestCases(t, []TestCase{
+			{"testdata/terminal/terminal4-1.json", processing.TerminalEffect{SeqNo: 0, TerminalName: "default", Command: "cd hello", Output: nil, CurrentDirectory: internal.Address("hello")}},
+		})
+	})
 
-func TestTerminal_WriteOutput(t *testing.T) {
-	terminal := processing.NewTerminalProcessor("default")
-	terminal.WriteCommand("echo abc")
-	terminal.WriteOutput("abc")
-	result := terminal.ToGraphQLTerminal()
-	internal.CompareAfterMarshal(t, "testdata/terminal/write-output1.json", result)
-}
-
-func TestTerminal_ChangeCurrentDirectory1(t *testing.T) {
-	terminal := processing.NewTerminalProcessor("default")
-	terminal.WriteCommand("cd hello")
-	terminal.ChangeCurrentDirectory("hello")
-	result := terminal.ToGraphQLTerminal()
-	internal.CompareAfterMarshal(t, "testdata/terminal/cd1.json", result)
-}
-
-func TestTerminal_ChangeCurrentDirectory2(t *testing.T) {
-	terminal := processing.NewTerminalProcessor("default")
-	terminal.WriteCommand("cd hello/world/thunder")
-	terminal.ChangeCurrentDirectory("hello/world/thunder")
-	result := terminal.ToGraphQLTerminal()
-	internal.CompareAfterMarshal(t, "testdata/terminal/cd2.json", result)
+	t.Run("change_current_directory2", func(t *testing.T) {
+		runTestCases(t, []TestCase{
+			{"testdata/terminal/terminal5-1.json", processing.TerminalEffect{SeqNo: 0, TerminalName: "default", Command: "cd hello/world/thunder", Output: nil, CurrentDirectory: internal.Address("hello/world/thunder")}},
+		})
+	})
 }
 
 func TestTerminal_Mutation1(t *testing.T) {
