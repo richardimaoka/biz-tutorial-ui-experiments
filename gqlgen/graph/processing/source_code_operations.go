ee65bc3883cca897b36c24366120a083fe30a88a
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-14 00:30:56 +0900

    file operations don't marshal to or unmarshal from json

--- gqlgen/graph/processing/source_code_operations.go
+++ gqlgen/graph/processing/source_code_operations.go
@@ -1,12 +1,5 @@
 package processing
 
-import (
-	"encoding/json"
-	"fmt"
-
-	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/graph/internal"
-)
-
 type SourceCodeOperation struct {
 	FileOps []FileSystemOperation
 }
@@ -20,32 +13,32 @@ type FileSystemOperation interface {
 }
 
 type DirectoryAdd struct {
-	FilePath string `json:"filePath"`
+	FilePath string
 }
 
 type DirectoryDelete struct {
-	FilePath string `json:"filePath"`
+	FilePath string
 }
 
 type FileAdd struct {
-	FilePath      string `json:"filePath"`
-	Content       string `json:"content"`
-	IsFullContent bool   `json:"isFullContent"`
+	FilePath      string
+	Content       string
+	IsFullContent bool
 }
 
 type FileUpdate struct {
-	FilePath string `json:"filePath"`
-	Content  string `json:"content"`
+	FilePath string
+	Content  string
 }
 
 type FileDelete struct {
-	FilePath string `json:"filePath"`
+	FilePath string
 }
 
 type FileUpsert struct {
-	FilePath      string `json:"filePath"`
-	Content       string `json:"content"`
-	IsFullContent bool   `json:"isFullContent"`
+	FilePath      string
+	Content       string
+	IsFullContent bool
 }
 
 func (o DirectoryAdd) IsFileSystemOperation()    {}
@@ -53,88 +46,3 @@ func (o DirectoryDelete) IsFileSystemOperation() {}
 func (o FileAdd) IsFileSystemOperation()         {}
 func (o FileUpdate) IsFileSystemOperation()      {}
 func (o FileDelete) IsFileSystemOperation()      {}
-
-//marshal DirectoryAdd to json string
-func (o DirectoryAdd) MarshalJSON() ([]byte, error) {
-	typeName := "DirectoryAdd"
-	m := make(map[string]interface{})
-	m["operationType"] = &typeName
-	m["filePath"] = o.FilePath
-
-	return json.Marshal(m)
-}
-
-func (o DirectoryDelete) MarshalJSON() ([]byte, error) {
-	typeName := "DirectoryDelete"
-
-	m := make(map[string]interface{})
-	m["operationType"] = &typeName
-	m["filePath"] = o.FilePath
-
-	return json.Marshal(m)
-}
-
-func (o FileAdd) MarshalJSON() ([]byte, error) {
-	typeName := "FileAdd"
-
-	m := make(map[string]interface{})
-	m["operationType"] = &typeName
-	m["filePath"] = o.FilePath
-	m["content"] = o.Content
-	m["isFullContent"] = o.IsFullContent
-
-	return json.Marshal(m)
-}
-
-func (o FileUpdate) MarshalJSON() ([]byte, error) {
-	typeName := "FileUpdate"
-
-	m := make(map[string]interface{})
-	m["operationType"] = &typeName
-	m["filePath"] = o.FilePath
-	m["content"] = o.Content
-
-	return json.Marshal(m)
-}
-
-func (o FileDelete) MarshalJSON() ([]byte, error) {
-	typeName := "FileDelete"
-
-	m := make(map[string]interface{})
-	m["operationType"] = &typeName
-	m["filePath"] = o.FilePath
-
-	return json.Marshal(m)
-}
-
-func unmarshalFileSystemOperation(bytes []byte) (FileSystemOperation, error) {
-	typeName, err := internal.ExtractTypeName(bytes, "operationType")
-	if err != nil {
-		return nil, fmt.Errorf("unmarshalAction() failed to extract operationType %s", err)
-	}
-
-	switch typeName {
-	case "FileAdd":
-		var op FileAdd
-		err := json.Unmarshal(bytes, &op)
-		return op, err
-	case "FileUpdate":
-		var op FileUpdate
-		err := json.Unmarshal(bytes, &op)
-		return op, err
-	case "FileDelete":
-		var op FileDelete
-		err := json.Unmarshal(bytes, &op)
-		return op, err
-	case "DirectoryAdd":
-		var op DirectoryAdd
-		err := json.Unmarshal(bytes, &op)
-		return op, err
-	case "DirectoryDelete":
-		var op DirectoryDelete
-		err := json.Unmarshal(bytes, &op)
-		return op, err
-	default:
-		return nil, fmt.Errorf("unmarshalFileSystemOperation() found invalid operationType = %s", typeName)
-	}
-}
