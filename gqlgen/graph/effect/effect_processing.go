f7f6c38a0d208a84d96f917b228d2d0d8e916b92
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-14 11:33:09 +0900

    GitEffectProcessing in progress

--- gqlgen/graph/effect/effect_processing.go
+++ gqlgen/graph/effect/effect_processing.go
@@ -8,43 +8,50 @@ import (
 	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/graph/processing"
 )
 
-// func GitEffectProcessing() error {
-// 	// 1. read effects from repository
-// 	log.Printf("EffectProcessing started")
-
-// 	stepEffects, err := GitStepEffects(nil)
-// 	if err != nil {
-// 		return fmt.Errorf("GitStepEffects failed: %v", err)
-// 	}
-
-// 	// 3. apply page-state operation and write states to files
-// 	state := processing.NewPageStateProcessor()
-// 	for i, step := range stepEffects {
-// 		state.TransitionToNext()
-// 		op, err := stepEffects[i].ToOperation()
-// 		if err != nil {
-// 			return fmt.Errorf("ToOperation() in PageStateEffect failed: %v", err)
-// 		}
-
-// 		// after registering the next op, write to the file
-// 		nextStep := step.NextStep
-// 		state.RegisterNext(nextStep, &op)
-// 		WriteJsonToFile(state.ToGraphQLPageState(), fmt.Sprintf("data/state/page-state%s.json", stepEffects[i].CurrentStep))
-
-// 		// iterate over to the next state
-// 		state.TransitionToNext()
-// 	}
-// 	// last state writes to the file
-// 	lastStep := stepEffects[len(stepEffects)-1].CurrentStep
-// 	WriteJsonToFile(state.ToGraphQLPageState(), fmt.Sprintf("data/state/page-state%s.json", lastStep))
-
-// 	return nil
-// }
-
-func EffectProcessing() error {
-	// 1. read effects from files
+func GitEffectProcessing() error {
+	//----------------------------------
+	// 1. read effects from git repository
+	//------------------------------------
 	log.Printf("EffectProcessing started")
 
+	stepEffects, err := GitStepEffects(nil)
+	if err != nil {
+		return fmt.Errorf("GitStepEffects failed: %v", err)
+	}
+
+	//--------------------------------------------------------
+	// 2. apply page-state operation and write states to files
+	//--------------------------------------------------------
+	state := processing.NewPageStateProcessor()
+	for i, step := range stepEffects {
+		state.TransitionToNext()
+		var op processing.PageStateOperation
+		// op, err := stepEffects[i].ToOperation()
+		// if err != nil {
+		// 	return fmt.Errorf("ToOperation() in PageStateEffect failed: %v", err)
+		// }
+
+		// // after registering the next op, write to the file
+		nextStep := step.NextStep
+		state.RegisterNext(nextStep, &op)
+		internal.WriteJsonToFile(state.ToGraphQLPageState(), fmt.Sprintf("data/state/page-state%s.json", stepEffects[i].CurrentStep))
+
+		// iterate over to the next state
+		state.TransitionToNext()
+	}
+	// last state writes to the file
+	lastStep := stepEffects[len(stepEffects)-1].CurrentStep
+	internal.WriteJsonToFile(state.ToGraphQLPageState(), fmt.Sprintf("data/state/page-state%s.json", lastStep))
+
+	return nil
+}
+
+func EffectProcessing() error {
+	log.Printf("EffectProcessing started")
+
+	//---------------------------
+	// 1. read effects from files
+	//---------------------------
 	stepEffects, err := ReadStepEffects("data/step-effects.json")
 	if err != nil {
 		return fmt.Errorf("EffectProcessing failed: %v", err)
@@ -63,7 +70,9 @@ func EffectProcessing() error {
 	}
 	log.Printf("%d terminal effects read ", len(terminalEffects))
 
+	//------------------------------
 	// 2. construct page-sate effect
+	//------------------------------
 	var pageStateEffects []PageStateEffect
 	for _, step := range stepEffects {
 		// SourceCodeEffect for seqNo
@@ -79,7 +88,9 @@ func EffectProcessing() error {
 	}
 	log.Printf("%d page state effects calculated", len(pageStateEffects))
 
+	//--------------------------------------------------------
 	// 3. apply page-state operation and write states to files
+	//--------------------------------------------------------
 	state := processing.NewPageStateProcessor()
 	for i, step := range stepEffects {
 		op, err := pageStateEffects[i].ToOperation()
@@ -100,6 +111,5 @@ func EffectProcessing() error {
 	internal.WriteJsonToFile(state.ToGraphQLPageState(), fmt.Sprintf("data/state/page-state%s.json", lastStep))
 
 	log.Printf("finished writing state into files")
-
 	return nil
 }
