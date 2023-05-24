a145f1eab15955d7689d04d6203764fbb56f15b9
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-25 06:52:26 +0900

    split markdown css

--- next/components/markdown/MarkdownPane.tsx
+++ next/components/markdown/MarkdownPane.tsx
@@ -16,6 +16,83 @@ export const MarkdownPane = () => {
   optimistic?: boolean;
 }`;
 
+  const markdownCss = css`
+    background-color: ${dark1MainBg};
+
+    h1 {
+      font-size: 32px;
+      font-weight: 700;
+      margin: 21px 0px;
+    }
+
+    h2 {
+      font-size: 24px;
+      font-weight: 700;
+      margin: 19px 0px;
+    }
+
+    h3 {
+      font-size: 18px;
+      font-weight: 700;
+      margin: 18px 0px;
+    }
+
+    p {
+      font-size: 14px;
+      margin: 16px 0px;
+    }
+
+    ul {
+      margin: 16px 0px;
+    }
+
+    ul > li {
+      font-size: 14px;
+      margin: 4px 0px;
+    }
+
+    code {
+      font-family: "Roboto Mono";
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 18px;
+      padding: 1px 4px;
+      background-color: ${dark5};
+    }
+
+    pre {
+      margin: 16px 0px;
+      padding: 8px;
+      background-color: ${dark5};
+    }
+
+    pre > code {
+      font-size: 14px;
+      line-height: 20px;
+    }
+
+    blockquote {
+      margin: 16px 0px;
+      margin: 0px;
+      padding-left: 18px;
+      border-left: 2px solid ${themeBlue};
+      color: ${gray};
+    }
+
+    img {
+      margin: 16px 0px;
+      display: block;
+      margin: 0 auto;
+      max-width: 100%;
+    }
+
+    iframe {
+      display: block;
+      margin: 0 auto;
+      max-width: 100%;
+    }
+  `;
+
   return (
     <div
       css={css`
@@ -27,82 +104,7 @@ export const MarkdownPane = () => {
         // options.components (https://github.com/rehypejs/rehype-react#optionscomponents) to avoid the following Next Lint error:
         // > Using `<img>` could result in slower LCP and higher bandwidth. Use `<Image />` from `next/image` instead to utilize Image Optimization.
         // > See: https://nextjs.org/docs/messages/no-img-elementeslint@next/next/no-img-element
-        css={css`
-          background-color: ${dark1MainBg};
-
-          h1 {
-            font-size: 32px;
-            font-weight: 700;
-            margin: 21px 0px;
-          }
-
-          h2 {
-            font-size: 24px;
-            font-weight: 700;
-            margin: 19px 0px;
-          }
-
-          h3 {
-            font-size: 18px;
-            font-weight: 700;
-            margin: 18px 0px;
-          }
-
-          p {
-            font-size: 14px;
-            margin: 16px 0px;
-          }
-
-          ul {
-            margin: 16px 0px;
-          }
-
-          ul > li {
-            font-size: 14px;
-            margin: 4px 0px;
-          }
-
-          code {
-            font-family: "Roboto Mono";
-            font-weight: 500;
-            font-size: 14px;
-            line-height: 18px;
-            padding: 1px 4px;
-            background-color: ${dark5};
-          }
-
-          pre {
-            margin: 16px 0px;
-            padding: 8px;
-            background-color: ${dark5};
-          }
-
-          pre > code {
-            font-size: 14px;
-            line-height: 20px;
-          }
-
-          blockquote {
-            margin: 16px 0px;
-            margin: 0px;
-            padding-left: 18px;
-            border-left: 2px solid ${themeBlue};
-            color: ${gray};
-          }
-
-          img {
-            margin: 16px 0px;
-            display: block;
-            margin: 0 auto;
-            max-width: 100%;
-          }
-
-          iframe {
-            display: block;
-            margin: 0 auto;
-            max-width: 100%;
-          }
-        `}
+        css={markdownCss}
       >
         <h1>Get started with Apollo Client</h1>
         <p>
