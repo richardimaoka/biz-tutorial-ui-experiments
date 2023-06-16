c9f3ceeb390488de674baba71969101f15f9930c
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-06-16 09:52:40 +0900

    file type detection and highliths in graphql

--- gqlgen/preprocess/processing/file_node.go
+++ gqlgen/preprocess/processing/file_node.go
@@ -133,18 +133,124 @@ func (n *directoryProcessorNode) Clone() fileTreeNode {
 	return &copied
 }
 
+func (n *fileProcessorNode) language() string {
+	split := strings.Split(n.filePath, ".")
+
+	if len(split) == 1 {
+		return ""
+	}
+
+	suffix := split[len(split)-1]
+	switch suffix {
+	case "go":
+		return "go"
+	case "js":
+		return "javascript"
+	case "ts":
+		return "typescript"
+	case "html":
+		return "html"
+	case "css":
+		return "css"
+	case "md":
+		return "markdown"
+	case "json":
+		return "json"
+	case "yaml":
+		return "yaml"
+	case "yml":
+		return "yaml"
+	case "graphql":
+		return "graphql"
+	case "gql":
+		return "graphql"
+	case "sql":
+		return "sql"
+	case "py":
+		return "python"
+	case "java":
+		return "java"
+	case "kt":
+		return "kotlin"
+	case "swift":
+		return "swift"
+	case "rb":
+		return "ruby"
+	case "php":
+		return "php"
+	case "c":
+		return "c"
+	case "cpp":
+		return "cpp"
+	case "h":
+		return "cpp"
+	case "hpp":
+		return "cpp"
+	case "cs":
+		return "csharp"
+	case "scala":
+		return "scala"
+	case "rs":
+		return "rust"
+	case "sh":
+		return "shell"
+	case "bash":
+		return "shell"
+	case "zsh":
+		return "shell"
+	case "ps1":
+		return "powershell"
+	case "psm1":
+		return "powershell"
+	case "bat":
+		return "batch"
+	case "gradle":
+		return "groovy"
+	case "xml":
+		return "xml"
+	case "vue":
+		return "vue"
+	default:
+		return ""
+	}
+}
+
+func (h *fileHighlight) ToGraphQLFileHighlight() *model.FileHighlight {
+	fromLine := h.fromLine // copy to avoid mutation effect afterwards
+	toLine := h.toLine     // copy to avoid mutation effect afterwards
+
+	return &model.FileHighlight{
+		FromLine: &fromLine,
+		ToLine:   &toLine,
+	}
+}
+
 func (n *fileProcessorNode) ToGraphQLOpenFile() *model.OpenFile {
 	filePath := n.filePath // copy to avoid mutation effect afterwards
 	content := n.content   // copy to avoid mutation effect afterwards
 	isFullContent := true
 	split := strings.Split(filePath, "/")
 
+	l := n.language()
+	var language *string
+	if l != "" {
+		language = &l
+	} else {
+		language = nil
+	}
+
+	var highlights []*model.FileHighlight
+	for _, h := range n.highlights {
+		highlight := h.ToGraphQLFileHighlight()
+		highlights = append(highlights, highlight)
+	}
+
 	return &model.OpenFile{
 		FilePath:      &filePath,
 		FileName:      &split[len(split)-1],
 		Content:       &content,
 		IsFullContent: &isFullContent,
-		Language:      nil,
-		Highlight:     nil,
+		Language:      language,
+		Highlight:     highlights,
 	}
 }
