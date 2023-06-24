d2df3bfaa39d9c894846a26828255ced3460c543
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-06-24 21:47:17 +0900

    failing test

--- gqlgen/preprocess/processing/export_test.go
+++ gqlgen/preprocess/processing/export_test.go
@@ -3,3 +3,21 @@ package processing
 type FileHighlight = fileHighlight
 
 var CalcHighlight = calcHighlight
+
+type FileProcessorNode = fileProcessorNode
+
+type DirectoryProcessorNode = directoryProcessorNode
+
+type FileTreeNode = fileTreeNode
+
+func NewFileProcessorNode(filePath string) *FileProcessorNode {
+	return &FileProcessorNode{
+		filePath: filePath,
+	}
+}
+
+func NewDirectoryProcessorNode(filePath string) *DirectoryProcessorNode {
+	return &DirectoryProcessorNode{
+		filePath: filePath,
+	}
+}
---
+++ gqlgen/preprocess/processing/file_node_test.go
@@ -0,0 +1,33 @@
+package processing_test
+
+import (
+	"testing"
+
+	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/preprocess/processing"
+)
+
+func TestNodeLessFile(t *testing.T) {
+	type Entry struct {
+		node1 processing.FileTreeNode
+		node2 processing.FileTreeNode
+		less  bool
+	}
+
+	var entries []Entry = []Entry{
+		{
+			processing.NewFileProcessorNode("aaa"),
+			processing.NewFileProcessorNode("aaa"),
+			true,
+		},
+	}
+
+	for _, e := range entries {
+		if processing.LessFileNode(e.node1, e.node2) != e.less {
+			t.Errorf(
+				"%+v %s %+v is expected, but they did not make it",
+				e.node1,
+				comparisonLetter(e.less),
+				e.node2)
+		}
+	}
+}
