51b1041f3cf8ad341282d178210f9fa476ed7af6
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-08 09:30:12 +0900

    bg column

--- json-processor/effect/bg_column.go
+++ json-processor/effect/bg_column.go
@@ -7,7 +7,7 @@ import (
 	"github.com/richardimaoka/biz-tutorial-ui-experiments/json-processor/internal"
 )
 
-type ImageColumnEffect struct {
+type BackgroundColumnEffect struct {
 	SeqNo               int    `json:"seqNo"`
 	Column              int    `json:"column"`
 	Width               int    `json:"width"`
@@ -18,20 +18,20 @@ type ImageColumnEffect struct {
 	DescriptionContents string `json:"description.contents"`
 }
 
-type ImageColumnEffects []ImageColumnEffect
+type BackgroundColumnEffects []BackgroundColumnEffect
 
-func ReadImageColumnEffects(filePath string) (ImageColumnEffects, error) {
-	var effects ImageColumnEffects
+func ReadBackgroundColumnEffects(filePath string) (BackgroundColumnEffects, error) {
+	var effects BackgroundColumnEffects
 	unmarshaller := func(jsonBytes []byte) error { return json.Unmarshal(jsonBytes, &effects) }
 	err := internal.JsonRead(filePath, unmarshaller)
 	if err != nil {
-		return nil, fmt.Errorf("ReadImageColumnEffects failed to read file, %s", err)
+		return nil, fmt.Errorf("ReadBackgroundColumnEffects failed to read file, %s", err)
 	}
 
 	return effects, err
 }
 
-func (t ImageColumnEffects) FindBySeqNo(seqNo int) *ImageColumnEffect {
+func (t BackgroundColumnEffects) FindBySeqNo(seqNo int) *BackgroundColumnEffect {
 	for _, e := range t {
 		if e.SeqNo == seqNo {
 			return &e // found!
