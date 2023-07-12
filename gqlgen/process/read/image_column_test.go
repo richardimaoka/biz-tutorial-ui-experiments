8f80417d54989235e9efbb6264dcb2511e9f27b9
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-12 20:40:18 +0900

    img_column read

---
+++ gqlgen/process/read/image_column_test.go
@@ -0,0 +1,21 @@
+package read_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/internal"
+	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/process/read"
+)
+
+func TestReadImgColumn(t *testing.T) {
+	filepath := "testdata/bg_columns.json"
+	entries, err := read.ReadImageDescriptionColumns(filepath)
+	if err != nil {
+		t.Fatalf("ReadImageDescriptionColumns failed to read file, %s", err)
+	}
+
+	for i, e := range entries {
+		internal.CompareWitGoldenFile(t, *updateFlag, fmt.Sprintf("testdata/img_col_entry_golden%d.json", i), e)
+	}
+}
