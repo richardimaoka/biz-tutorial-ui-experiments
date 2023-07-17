f6860f611cea428cf580588ba8d9b6a9eb222fde
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-17 09:32:41 +0900

    move testdata

--- gqlgen/process/step_entries_test.go
+++ gqlgen/process/step_entries_test.go
@@ -8,20 +8,20 @@ import (
 )
 
 func TestReadStepEntries(t *testing.T) {
-	effects, err := process.ReadStepEntries("testdata")
+	effects, err := process.ReadStepEntries("testdata/basic")
 	if err != nil {
 		t.Fatalf("ReadStepEntries failed to read file, %s", err)
 	}
 
-	internal.CompareWitGoldenFile(t, *updateFlag, "testdata/golden/step_entries_golden.json", effects)
+	internal.CompareWitGoldenFile(t, *updateFlag, "testdata/golden/basic/step_entries_golden.json", effects)
 }
 
 func TestToGraphQLPages(t *testing.T) {
-	effects, err := process.ReadStepEntries("testdata")
+	effects, err := process.ReadStepEntries("testdata/basic")
 	if err != nil {
 		t.Fatalf("ReadStepEntries failed to read file, %s", err)
 	}
 
 	pages := effects.ToGraphQLPages()
-	internal.CompareWitGoldenFile(t, *updateFlag, "testdata/golden/pages_golden.json", pages)
+	internal.CompareWitGoldenFile(t, *updateFlag, "testdata/basic/golden/pages_golden.json", pages)
 }
