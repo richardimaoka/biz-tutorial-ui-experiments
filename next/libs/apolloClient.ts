ac20a91277ff9a56b1293860ee88869275e2bac1
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-19 06:44:31 +0900

    apollo client not to cache

--- next/libs/apolloClient.ts
+++ next/libs/apolloClient.ts
@@ -3,4 +3,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
   uri: "http://localhost:8080/query",
   cache: new InMemoryCache(),
+  defaultOptions: {
+    query: {
+      fetchPolicy: "no-cache",
+    },
+  },
 });
