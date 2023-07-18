4ae41042a38f14949e3193d13acc4db6c11cf3ee
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-19 06:34:20 +0900

    column positioning

--- next/components/column/ColumnWrapper.tsx
+++ next/components/column/ColumnWrapper.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
 import { css } from "@emotion/react";
+import { dark1MainBg } from "../../libs/colorTheme";
 import { FragmentType, graphql, useFragment } from "../../libs/gql";
 import { ImageDescriptionColumn } from "./ImageDescriptionColumn";
+import { ReactNode } from "react";
 
 const fragmentDefinition = graphql(`
   fragment ColumnWrapperFragment on ColumnWrapper {
@@ -17,6 +18,25 @@ export interface ColumnWrapperProps {
   fragment: FragmentType<typeof fragmentDefinition>;
 }
 
+const ColumnPositioning = ({ children }: { children: ReactNode }) => {
+  return (
+    <div
+      css={css`
+        @media (max-width: 768px) {
+          width: 100vw;
+          height: 100vh;
+        }
+        width: 768px;
+        min-height: 100vh;
+        overflow: auto;
+        background-color: ${dark1MainBg};
+      `}
+    >
+      {children}
+    </div>
+  );
+};
+
 export const ColumnWrapper = (props: ColumnWrapperProps): JSX.Element => {
   const fragment = useFragment(fragmentDefinition, props.fragment);
   const typename = fragment.column?.__typename;
@@ -31,7 +51,11 @@ export const ColumnWrapper = (props: ColumnWrapperProps): JSX.Element => {
 
   switch (typename) {
     case "ImageDescriptionColumn":
-      return <ImageDescriptionColumn fragment={fragment.column} />;
+      return (
+        <ColumnPositioning>
+          <ImageDescriptionColumn fragment={fragment.column} />
+        </ColumnPositioning>
+      );
     default:
       return <>no matching column</>;
   }
