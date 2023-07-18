892a4dc8bc697086523849672b34bf70604fa3a9
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-18 23:56:18 +0900

    remove processing

--- gqlgen/main.go
+++ gqlgen/main.go
@@ -1,40 +1,12 @@
 package main
 
 import (
-	"log"
 	"os"
 )
 
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "processing" {
-		cases := []struct {
-			dirName string
-			repoUrl string
-		}{
-			{
-				dirName: "data/apollo-client-getting-started",
-				repoUrl: "https://github.com/richardimaoka/apollo-client-getting-started",
-			},
-			{
-				dirName: "data/gqlgensandbox",
-				repoUrl: "https://github.com/richardimaoka/gqlgensandbox",
-			},
-			{
-				dirName: "data/protoc-go-experiments",
-			},
-		}
 
-		for _, input := range cases {
-			if input.repoUrl == "" {
-				if err := EffectProcessing(input.dirName); err != nil {
-					log.Print(err)
-				}
-			} else {
-				if err := GitEffectProcessing(input.dirName, input.repoUrl); err != nil {
-					log.Print(err)
-				}
-			}
-		}
 	} else {
 		Server()
 	}
