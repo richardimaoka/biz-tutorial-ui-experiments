c3056d307e457e698d40977782b5927fda10aa33
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-18 23:41:28 +0900

    remove page from Query for now

--- gqlgen/graph/schema.resolvers.go
+++ gqlgen/graph/schema.resolvers.go
@@ -39,50 +39,6 @@ func (r *queryResolver) PageState(ctx context.Context, step *string) (*model.Pag
 	return &pageState, nil
 }
 
-// Page is the resolver for the page field.
-func (r *queryResolver) Page(ctx context.Context, tutorial string, step *string) (*model.PageState, error) {
-	if tutorial == "" {
-		return nil, fmt.Errorf("tutorial name must be specified")
-	}
-
-	var stepStr string
-	if step == nil {
-		log.Printf("step is nil, reading first step for %s", tutorial)
-		var firstStep string
-		firstStepFile := fmt.Sprintf("data/%s/first-step.json", tutorial)
-		bytes, err := os.ReadFile(firstStepFile)
-		if err != nil {
-			return nil, fmt.Errorf("tutorial = %s does not exist", tutorial)
-		}
-		if err := json.Unmarshal(bytes, &firstStep); err != nil {
-			return nil, fmt.Errorf("tutorial = %s doesn't define a valid first step", tutorial)
-		}
-		if firstStep == "" {
-			log.Printf("first step from file = %s was empty", firstStepFile)
-			return nil, fmt.Errorf("tutorial = %s doesn't define a valid first step", tutorial)
-		}
-		stepStr = firstStep
-	} else {
-		stepStr = *step
-	}
-	filename := fmt.Sprintf("data/%s/state/state-%s.json", tutorial, stepStr)
-
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		log.Printf("failed to read %s, %s", filename, err)
-		return nil, fmt.Errorf("failed to read data for tutorial = %s, step = `%s`", tutorial, stepStr)
-	}
-
-	var pageState model.PageState
-	err = json.Unmarshal(data, &pageState)
-	if err != nil {
-		log.Printf("failed to read data from %s, %s", filename, err)
-		return nil, fmt.Errorf("internal server error %s", *step)
-	}
-
-	return &pageState, nil
-}
-
 // OpenFile is the resolver for the openFile field.
 func (r *sourceCodeResolver) OpenFile(ctx context.Context, obj *model.SourceCode, filePath *string) (*model.OpenFile, error) {
 	var filename string
@@ -138,6 +94,49 @@ type sourceCodeResolver struct{ *Resolver }
 //  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
 //    it when you're done.
 //  - You have helper methods in this file. Move them out to keep these resolver files clean.
+func (r *queryResolver) Page(ctx context.Context, tutorial string, step *string) (*model.PageState, error) {
+	if tutorial == "" {
+		return nil, fmt.Errorf("tutorial name must be specified")
+	}
+
+	var stepStr string
+	if step == nil {
+		log.Printf("step is nil, reading first step for %s", tutorial)
+		var firstStep string
+		firstStepFile := fmt.Sprintf("data/%s/first-step.json", tutorial)
+		bytes, err := os.ReadFile(firstStepFile)
+		if err != nil {
+			return nil, fmt.Errorf("tutorial = %s does not exist", tutorial)
+		}
+		if err := json.Unmarshal(bytes, &firstStep); err != nil {
+			return nil, fmt.Errorf("tutorial = %s doesn't define a valid first step", tutorial)
+		}
+		if firstStep == "" {
+			log.Printf("first step from file = %s was empty", firstStepFile)
+			return nil, fmt.Errorf("tutorial = %s doesn't define a valid first step", tutorial)
+		}
+		stepStr = firstStep
+	} else {
+		stepStr = *step
+	}
+	filename := fmt.Sprintf("data/%s/state/state-%s.json", tutorial, stepStr)
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Printf("failed to read %s, %s", filename, err)
+		return nil, fmt.Errorf("failed to read data for tutorial = %s, step = `%s`", tutorial, stepStr)
+	}
+
+	var pageState model.PageState
+	err = json.Unmarshal(data, &pageState)
+	if err != nil {
+		log.Printf("failed to read data from %s, %s", filename, err)
+		return nil, fmt.Errorf("internal server error %s", *step)
+	}
+
+	return &pageState, nil
+}
+
 var dirName = "data/apollo-client-getting-started/state"
 var initialStep = "_initial"
 
