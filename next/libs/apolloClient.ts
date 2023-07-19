1d25c64308808bab166fd3bc6c663239b446b3f8
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-20 02:20:12 +0900

    apollo link error handling

--- next/libs/apolloClient.ts
+++ next/libs/apolloClient.ts
@@ -1,7 +1,23 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:8080/query",
+});
 
 export const client = new ApolloClient({
-  uri: "http://localhost:8080/query",
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
   defaultOptions: {
     query: {
