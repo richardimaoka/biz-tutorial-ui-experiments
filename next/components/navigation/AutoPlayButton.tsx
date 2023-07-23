f5b89b859d9943c00ea048856bb0dad83589d5e4
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-23 12:23:31 +0900

    autoplay button adds a query parameter

--- next/components/navigation/AutoPlayButton.tsx
+++ next/components/navigation/AutoPlayButton.tsx
@@ -1,39 +1,48 @@
 import { css } from "@emotion/react";
+import { useRouter } from "next/router";
 
-export const AutoPlayButton = () => (
-  <button
-    css={css`
-      //positioning
-      position: fixed;
-      bottom: 0px;
-      left: 50%;
-      transform: translate(-50%, 0%);
+export const AutoPlayButton = () => {
+  const router = useRouter();
+  const onClick = () => {
+    router.replace({ query: { ...router.query, autoPlay: true } });
+  };
 
-      //sizing
-      width: 120px;
-      height: 40px;
-
-      // color and styles
-      background-color: rgba(255, 255, 255, 0.8);
-      color: black;
-      border-style: none;
-    `}
-  >
-    <div
+  return (
+    <button
       css={css`
-        font-size: 16px;
-        height: 18px;
+        //positioning
+        position: fixed;
+        bottom: 0px;
+        left: 50%;
+        transform: translate(-50%, 0%);
+
+        //sizing
+        width: 120px;
+        height: 40px;
+
+        // color and styles
+        background-color: rgba(255, 255, 255, 0.8);
+        color: black;
+        border-style: none;
       `}
+      onClick={onClick}
     >
-      Auto Play
-    </div>
-    <div
-      css={css`
-        font-size: 8px;
-        line-height: 8px;
-      `}
-    >
-      to next milestone
-    </div>
-  </button>
-);
+      <div
+        css={css`
+          font-size: 16px;
+          height: 18px;
+        `}
+      >
+        Auto Play
+      </div>
+      <div
+        css={css`
+          font-size: 8px;
+          line-height: 8px;
+        `}
+      >
+        to next milestone
+      </div>
+    </button>
+  );
+};
