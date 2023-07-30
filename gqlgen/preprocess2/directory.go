4a1ca7c0cb583b9da35a273743d318cda0040455
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-31 08:23:01 +0900

    preprocess2 is not needed anymore, dir-based src state is already in process/state package

--- gqlgen/preprocess2/directory.go
+++
@@ -1,9 +0,0 @@
-package preprocess2
-
-import "github.com/go-git/go-git/v5/plumbing"
-
-type Directory struct {
-	dirs     []Directory
-	files    []File
-	treeHash plumbing.Hash //assuming every transition is a git commit
-}
--- gqlgen/preprocess2/step.go
+++
@@ -1,6 +0,0 @@
-package preprocess2
-
-type SourceCodeStep struct {
-	ID         string
-	commitHash string
-}
