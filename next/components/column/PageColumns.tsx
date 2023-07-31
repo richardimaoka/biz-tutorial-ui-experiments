7a9d108ed3763b37c3e4c6af7c1554d262c7e7cd
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-31 16:42:26 +0900

    tabs and 80vh

--- next/components/column/PageColumns.tsx
+++ next/components/column/PageColumns.tsx
@@ -21,65 +21,84 @@ const fragmentDefinition = graphql(`
   }
 `);
 
-export interface ColumnWrapperProps {
+const scrollBarStyle = css`
+  // scroll bar style
+  ::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    background-color: #252526; /* or add it to the track */
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #37373d;
+    border-radius: 8px;
+  }
+  ::-webkit-scrollbar-corner {
+    background-color: #252526;
+  }
+`;
+
+export interface PageColumnsProps {
   fragment: FragmentType<typeof fragmentDefinition>;
 }
 
-export const PageColumns = (props: ColumnWrapperProps): JSX.Element => {
+export const PageColumns = (props: PageColumnsProps): JSX.Element => {
   const fragment = useFragment(fragmentDefinition, props.fragment);
 
   if (!fragment.columns) {
     return <></>;
   }
+  const columns = nonNullArray(fragment.columns);
+
   if (!fragment.step) {
     return <></>;
   }
-
-  const columns = nonNullArray(fragment.columns);
+  const step = fragment.step;
 
   const desktopColumnWidth = 768;
   const desktopWidth = desktopColumnWidth;
 
-  const step = fragment.step;
-
-  const scrollBarStyle = css`
-    // scroll bar style
-    ::-webkit-scrollbar {
-      width: 8px;
-      height: 8px;
-      background-color: #252526; /* or add it to the track */
-    }
-    ::-webkit-scrollbar-thumb {
-      background: #37373d;
-      border-radius: 8px;
-    }
-    ::-webkit-scrollbar-corner {
-      background-color: #252526;
-    }
-  `;
-
   const Inner = () => (
-    <>
-      <div>{fragment.focusColumn}</div>
+    <div
+      css={css`
+        // on mobile, show one column only
+        @media (max-width: 768px) {
+          width: 100vw;
+        }
+        // on desktop, show one column ony
+        width: ${desktopWidth}px;
+        margin: 0 auto; // centering on desktop
+        height: 100svh;
+
+        overflow-y: hidden; // let inner column handle y-axis scroll
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          gap: 8px;
+        `}
+      >
+        {columns.map((col, index) => (
+          <div
+            key={col.name ? col.name : index}
+            css={css`
+              color: ${col.name === fragment.focusColumn ? "white" : "gray"};
+            `}
+          >
+            {col.name}
+          </div>
+        ))}
+      </div>
       <div
         css={css`
           // flex to allow multiple columns
           display: flex;
           gap: 20px;
 
-          // on mobile, show one column only
-          @media (max-width: 768px) {
-            width: 100vw;
-          }
-          // on desktop, show one column ony
-          width: ${desktopWidth}px;
-          margin: 0 auto; // centering on desktop
-          height: 80svh;
-
           // carousel container
           scroll-snap-type: x mandatory;
           scroll-behavior: smooth;
-          overflow-x: hidden; // buttons are the only way to scroll
+          overflow-x: auto; // buttons are the only way to scroll
           overflow-y: hidden; // let inner column handle y-axis scroll
           /* ${scrollBarStyle} */
         `}
@@ -99,12 +118,12 @@ export const PageColumns = (props: ColumnWrapperProps): JSX.Element => {
                 // on mobile, use full screen
                 @media (max-width: 768px) {
                   width: 100vw;
-                  height: 100vh;
+                  height: 80vh;
                 }
 
                 // on desktop, use fixed width
                 width: ${desktopColumnWidth}px;
-                height: 100vh;
+                height: 80vh;
 
                 // in-column scroll for y-axis
                 overflow-y: auto;
@@ -117,7 +136,7 @@ export const PageColumns = (props: ColumnWrapperProps): JSX.Element => {
           );
         })}
       </div>
-    </>
+    </div>
   );
 
   return fragment.modal ? (
