db9d0e32ae399a6dde16a0694f2842fb1d9edc65
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-10 03:00:50 +0900

    file added and deleted cases

--- gqlgen/process/state/file_test.go
+++ gqlgen/process/state/file_test.go
@@ -25,18 +25,27 @@ func gitOpenOrClone(repoUrl string) (*git.Repository, error) {
 	return repo, nil
 }
 
-func gitFileFromCommit(repoUrl, commitHashStr, filePath string) (*object.File, error) {
+func gitCommit(repoUrl, commitHashStr string) (*object.Commit, error) {
 	repo, err := gitOpenOrClone(repoUrl)
 	if err != nil {
-		return nil, fmt.Errorf("failed in gitFileFromCommit, %s", err)
+		return nil, fmt.Errorf("failed in gitCommit for, %s", err)
 	}
 
 	commitHash := plumbing.NewHash(commitHashStr)
 	if commitHash.String() != commitHashStr {
-		return nil, fmt.Errorf("failed in gitFileFromCommit, commit hash = %s is invalid as its re-calculated hash is mismatched = %s", commitHashStr, commitHash.String())
+		return nil, fmt.Errorf("failed in gitCommit, commit hash = %s is invalid as its re-calculated hash is mismatched = %s", commitHashStr, commitHash.String())
 	}
 
 	commit, err := repo.CommitObject(commitHash)
+	if err != nil {
+		return nil, fmt.Errorf("failed in gitCommit, cannot get commit = %s, %s", commitHashStr, err)
+	}
+
+	return commit, nil
+}
+
+func gitFileFromCommit(repoUrl, commitHashStr, filePath string) (*object.File, error) {
+	commit, err := gitCommit(repoUrl, commitHashStr)
 	if err != nil {
 		return nil, fmt.Errorf("failed in gitFileFromCommit, cannot get commit = %s, %s", commitHashStr, err)
 	}
@@ -70,6 +79,8 @@ func fileStateFromCommit(repoUrl, commitHashStr, filePath string) (*state.File, 
 }
 
 func TestFileUnchanged(t *testing.T) {
+	repoUrl := "https://github.com/richardimaoka/next-sandbox.git"
+
 	cases := []struct {
 		commit             string
 		filePath           string
@@ -106,7 +117,6 @@ func TestFileUnchanged(t *testing.T) {
 		},
 	}
 
-	repoUrl := "https://github.com/richardimaoka/next-sandbox.git"
 	for _, c := range cases {
 		gitFile, err := gitFileFromCommit(repoUrl, c.commit, c.filePath)
 		if err != nil {
@@ -121,6 +131,100 @@ func TestFileUnchanged(t *testing.T) {
 		internal.CompareWitGoldenFile(t, *updateFlag, c.goldenFileOpenFile, s.ToGraphQLOpenFile())
 		internal.CompareWitGoldenFile(t, *updateFlag, c.goldenFileFileNode, s.ToGraphQLFileNode())
 	}
+}
+
+func TestFileAdded(t *testing.T) {
+	repoUrl := "https://github.com/richardimaoka/next-sandbox.git"
+
+	cases := []struct {
+		prevCommit         string
+		currentCommit      string
+		filePath           string
+		goldenFileOpenFile string
+		goldenFileFileNode string
+	}{
+		{
+			// text file
+			"55c98498a85f4503e3922586ceeb86ab5100e91f", //cleanup
+			"8adac375628219e020d4b5957ff24f45954cbd3f", //npx create-next-app@latest
+			"next/package.json",
+			"testdata/file_added_openfile_golden1.json",
+			"testdata/file_added_filenode_golden1.json",
+		},
+	}
+
+	for _, c := range cases {
+		gitFile, err := gitFileFromCommit(repoUrl, c.currentCommit, c.filePath)
+		if err != nil {
+			t.Fatalf("failed in TestFileAdded to get gitFile, %s", err)
+		}
+
+		s, err := state.FileAdded(gitFile, "") //curretDir = "", as gitFile is retrieved with respect to the rootDir
+		if err != nil {
+			t.Fatalf("failed in TestFileAdded to create state.File, %s", err)
+		}
+
+		internal.CompareWitGoldenFile(t, *updateFlag, c.goldenFileOpenFile, s.ToGraphQLOpenFile())
+		internal.CompareWitGoldenFile(t, *updateFlag, c.goldenFileFileNode, s.ToGraphQLFileNode())
+	}
+}
+
+func TestFileDeleted(t *testing.T) {
+	repoUrl := "https://github.com/richardimaoka/next-sandbox.git"
+
+	cases := []struct {
+		prevCommit         string
+		currentCommit      string
+		filePath           string
+		goldenFileOpenFile string
+		goldenFileFileNode string
+	}{
+		{
+			// text file
+			"3b452151c8a567e2d42a133c255e85d81ea5912a", // getStaticProps
+			"55c98498a85f4503e3922586ceeb86ab5100e91f", //cleanup
+			".gitignore",
+			"testdata/file_deleted_openfile_golden1.json",
+			"testdata/file_deleted_filenode_golden1.json",
+		},
+	}
+
+	for _, c := range cases {
+		prevCommit, err := gitCommit(repoUrl, c.prevCommit)
+		if err != nil {
+			t.Fatalf("failed in TestFileDeleted to get prev commit, %s", err)
+		}
+		currentCommit, err := gitCommit(repoUrl, c.currentCommit)
+		if err != nil {
+			t.Fatalf("failed in TestFileDeleted to get current commit, %s", err)
+		}
+
+		patch, err := prevCommit.Patch(currentCommit)
+		if err != nil {
+			t.Fatalf("failed in TestFileDeleted to get patch, %s", err)
+		}
+
+		var deleted bool = false
+		for _, p := range patch.FilePatches() {
+			from, to := p.Files()
+			if from.Path() == c.filePath && to == nil {
+				s, err := state.FileDeleted(from)
+				if err != nil {
+					t.Fatalf("failed in TestFileDeleted to create state.File, %s", err)
+				}
+
+				internal.CompareWitGoldenFile(t, *updateFlag, c.goldenFileOpenFile, s.ToGraphQLOpenFile())
+				internal.CompareWitGoldenFile(t, *updateFlag, c.goldenFileFileNode, s.ToGraphQLFileNode())
+
+				deleted = true
+			}
+		}
+
+		if !deleted {
+			t.Fatalf("failed in TestFileDeleted, %s is not actually deleted between %s and %s", c.filePath, c.prevCommit, c.currentCommit)
+		}
+	}
+
 }
 
 func TestFilePatterns(t *testing.T) {
