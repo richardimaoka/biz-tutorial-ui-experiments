62a18bd6069e33bb28ebf237d353aebc06781a01
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-14 21:20:52 +0900

    md to reflect vertical alignment

--- next/app/components/markdown/MarkdownColumn.tsx
+++ next/app/components/markdown/MarkdownColumn.tsx
@@ -1,5 +1,6 @@
 import { FragmentType, graphql, useFragment } from "@/libs/gql";
 import { MarkdownView } from "../markdown/MarkdownView";
+import styles from "./style.module.css";
 
 const fragmentDefinition = graphql(`
   fragment MarkdownColumn_Fragment on MarkdownColumn {
@@ -16,11 +17,26 @@ export interface MarkdownColumnProps {
 
 export const MarkdownColumn = (props: MarkdownColumnProps): JSX.Element => {
   const fragment = useFragment(fragmentDefinition, props.fragment);
-  const position = fragment.contentsPosition
-    ? fragment.contentsPosition
-    : "TOP";
+  console.log(fragment);
+
+  const positionStyle = () => {
+    switch (fragment.contentsPosition) {
+      case "TOP":
+        return styles.verticalTop;
+      case "CENTER":
+        return styles.verticalCenter;
+      case "BOTTOM":
+        return styles.verticalBottom;
+      default:
+        return styles.verticaltop;
+    }
+  };
 
   if (!fragment.description) return <></>;
 
-  return <MarkdownView fragment={fragment.description} />;
+  return (
+    <div className={positionStyle()}>
+      <MarkdownView fragment={fragment.description} />
+    </div>
+  );
 };
