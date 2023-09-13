c9a03c3c6947696995dca888e2e07db75b8de8d9
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-13 21:58:55 +0900

    table based test

--- gqlgen/process/rough/steps_test.go
+++ gqlgen/process/rough/steps_test.go
@@ -10,47 +10,26 @@ import (
 )
 
 func TestRough(t *testing.T) {
-	filename := "testdata/rough-step.json"
-	goldenFile := "testdata/detailed-steps-golden.json"
-
-	bytes, err := os.ReadFile(filename)
-	if err != nil {
-		t.Fatalf("failed to read file: %v", err)
+	cases := []struct {
+		roughStepFile string
+		goldenFile    string
+	}{
+		{"testdata/rough-step.json", "testdata/detailed-steps-golden.json"},
 	}
 
-	var roughStep rough.RoughStep
-	err = json.Unmarshal(bytes, &roughStep)
-	if err != nil {
-		t.Fatalf("failed to unmarshal json: %v", err)
-	}
+	for _, c := range cases {
+		bytes, err := os.ReadFile(c.roughStepFile)
+		if err != nil {
+			t.Fatalf("failed to read file: %v", err)
+		}
 
-	result := roughStep.Convert("c8238063-dd5a-4cd4-9d62-5c9c8ebd35ec", []string{})
-	internal.CompareWitGoldenFile(t, *updateFlag, goldenFile, result)
+		var roughStep rough.RoughStep
+		err = json.Unmarshal(bytes, &roughStep)
+		if err != nil {
+			t.Fatalf("failed to unmarshal json: %v", err)
+		}
+
+		result := roughStep.Convert("c8238063-dd5a-4cd4-9d62-5c9c8ebd35ec", []string{})
+		internal.CompareWitGoldenFile(t, *updateFlag, c.goldenFile, result)
+	}
 }
-
-// func TestRough1(t *testing.T) {
-// 	cases := []struct {
-// 		rough    rough.RoughStep
-// 		expected []rough.DetailedStep
-// 	}{
-// 		{
-// 			rough: rough.RoughStep{
-// 				Instruction: "mkdir gqlgen-todos",
-// 			},
-// 			expected: []rough.DetailedStep{{
-// 				FocusColumn:  "Terminal",
-// 				TerminalText: "mkdir gqlgen-todos",
-// 				TerminalType: "command",
-// 			}},
-// 		},
-// 	}
-
-// 	for _, c := range cases {
-// 		steps := c.rough.Convert()
-// 		if steps[0] != c.expected[0] {
-// 			fmt.Printf("expected: %+v\n", c.expected[0])
-// 			fmt.Printf("actual: %+v\n", steps[0])
-// 			t.Fail()
-// 		}
-// 	}
-// }
