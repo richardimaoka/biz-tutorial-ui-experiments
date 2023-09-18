f82b746240f390df00791ac652ef2f1bcfeee229
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-19 08:11:16 +0900

    put things into a single .go file

--- gqlgen/process/rough/terminal.go
+++ gqlgen/process/rough/terminal.go
@@ -7,6 +7,48 @@ import (
 	"github.com/go-git/go-git/v5"
 )
 
+func (s *RoughStep) CommitConvert(state *InnerState, repo *git.Repository) ([]DetailedStep, error) {
+	var detailedSteps []DetailedStep
+
+	if s.Commit == "" {
+		return nil, fmt.Errorf("commit is missing for manual commit, phase = '%s', type = '%s', comment = '%s'", s.Phase, s.Type, s.Comment)
+	}
+
+	files, err := gitFilesForCommit(repo, s.Commit)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get files for commit = %s, %s", s.Commit, err)
+	}
+	if len(files) == 0 {
+		return nil, fmt.Errorf("failed to get files for commit = %s, no files found", s.Commit)
+	}
+
+	if state.currentCol != "Source Code" {
+		fileTreeStep := DetailedStep{
+			FocusColumn:         "Source Code",
+			IsFoldFileTree:      false,
+			DefaultOpenFilePath: files[0],
+			Commit:              s.Commit,
+		}
+		detailedSteps = append(detailedSteps, fileTreeStep)
+	}
+
+	// 3.2. file steps
+	for i, file := range files {
+		commitStep := DetailedStep{
+			FocusColumn:         "Source Code",
+			DefaultOpenFilePath: file,
+			IsFoldFileTree:      true,
+		}
+		detailedSteps = append(detailedSteps, commitStep)
+
+		if i == 5 {
+			break
+		}
+	}
+
+	return detailedSteps, nil
+}
+
 func (s *RoughStep) TerminalConvert(state *InnerState, repo *git.Repository) ([]DetailedStep, error) {
 	var detailedSteps []DetailedStep
 
@@ -50,6 +92,42 @@ func (s *RoughStep) TerminalConvert(state *InnerState, repo *git.Repository) ([]
 			return nil, fmt.Errorf("failed to convert commit steps, %s", err)
 		}
 		detailedSteps = append(detailedSteps, commitSteps...)
+	}
+
+	return detailedSteps, nil
+}
+
+func (s *RoughStep) SourceErrorConvert(state *InnerState, repo *git.Repository) ([]DetailedStep, error) {
+	var detailedSteps []DetailedStep
+
+	sourceErrorStep := DetailedStep{
+		FocusColumn:         "Source Code",
+		DefaultOpenFilePath: s.Instruction, // Go zero value is ""
+	}
+
+	detailedSteps = append(detailedSteps, sourceErrorStep)
+
+	return detailedSteps, nil
+}
+
+func (s *RoughStep) BrowserConvert(state *InnerState, repo *git.Repository) ([]DetailedStep, error) {
+	var detailedSteps []DetailedStep
+
+	if s.Instruction == "" {
+		browserStep := DetailedStep{
+			FocusColumn: "Browser",
+		}
+		detailedSteps = append(detailedSteps, browserStep)
+	} else {
+		split := strings.Split(s.Instruction, ",")
+		for _, s := range split {
+			browserImageName := strings.ReplaceAll(s, " ", "")
+			browserStep := DetailedStep{
+				FocusColumn:      "Browser",
+				BrowserImageName: browserImageName,
+			}
+			detailedSteps = append(detailedSteps, browserStep)
+		}
 	}
 
 	return detailedSteps, nil
