051f6999bd0615e8e5a24e3cc98a30289b0826ed
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-19 05:52:04 +0900

    source code file steps

--- gqlgen/process/rough/terminal.go
+++ gqlgen/process/rough/terminal.go
@@ -23,21 +23,57 @@ func (s *RoughStep) TerminalConvert(state *InnerState, repo *git.Repository) ([]
 	}
 	// 1.2. create command step
 	cmdStep := DetailedStep{
-		TerminalText: s.Instruction,
+		FocusColumn:  "Terminal",
 		TerminalType: "command",
+		TerminalText: s.Instruction,
+		TerminalName: s.Instruction3, // Go zero value is ""
 		CurrentDir:   currentDir,     // Go zero value is ""
 		Commit:       s.Commit,       // Go zero value is ""
-		TerminalName: s.Instruction3, // Go zero value is ""
 	}
 	detailedSteps = append(detailedSteps, cmdStep)
 
 	// 2. output step
 	if s.Instruction2 != "" {
 		outputStep := DetailedStep{
-			TerminalText: s.Instruction2,
+			FocusColumn:  "Terminal",
 			TerminalType: "output",
+			TerminalText: s.Instruction2,
 		}
 		detailedSteps = append(detailedSteps, outputStep)
+	}
+
+	// 3. source code steps
+	if s.Commit != "" {
+		files, err := gitFilesForCommit(repo, s.Commit)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get files for commit = %s, %s", s.Commit, err)
+		}
+		if len(files) == 0 {
+			return nil, fmt.Errorf("failed to get files for commit = %s, no files found", s.Commit)
+		}
+
+		// 3.1. file tree step
+		sourceCodeStep := DetailedStep{
+			FocusColumn:         "Source Code",
+			IsFoldFileTree:      false,
+			DefaultOpenFilePath: files[0],
+			Commit:              s.Commit,
+		}
+		detailedSteps = append(detailedSteps, sourceCodeStep)
+
+		// 3.2. file steps
+		for i, file := range files {
+			commitStep := DetailedStep{
+				FocusColumn:         "Source Code",
+				DefaultOpenFilePath: file,
+				IsFoldFileTree:      true,
+			}
+			detailedSteps = append(detailedSteps, commitStep)
+
+			if i == 5 {
+				break
+			}
+		}
 	}
 
 	return detailedSteps, nil
