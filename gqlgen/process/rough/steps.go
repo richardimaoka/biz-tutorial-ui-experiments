48b73bd35c1026514844db24b64b8fc5107c06df
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-18 18:59:43 +0900

    gitFilesForCommit

--- gqlgen/process/rough/steps.go
+++ gqlgen/process/rough/steps.go
@@ -2,7 +2,11 @@ package rough
 
 import (
 	"fmt"
+	"io"
 	"strings"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 )
 
 type InnerState struct {
@@ -137,6 +141,36 @@ func command(uuid, command, commit string, existingColumns []string) DetailedSte
 	}
 	step.setColumns(existingColumns, "Terminal")
 	return step
+}
+
+func gitFilesForCommit(repo *git.Repository, commitHashStr string) ([]string, error) {
+	commitHash := plumbing.NewHash(commitHashStr)
+	if commitHash.String() != commitHashStr {
+		return nil, fmt.Errorf("failed in gitFilesForCommit, commit hash = %s is invalid as its re-calculated hash is mismatched = %s", commitHashStr, commitHash.String())
+	}
+
+	commit, err := repo.CommitObject(commitHash)
+	if err != nil {
+		return nil, fmt.Errorf("failed in gitFilesForCommit, cannot get commit = %s, %s", commitHashStr, err)
+	}
+
+	fileIter, err := commit.Files()
+	if err != nil {
+		return nil, fmt.Errorf("failed in gitFilesForCommit, cannot get file iterator for commit = %s, %s", commitHashStr, err)
+	}
+
+	var files []string
+	for {
+		file, err := fileIter.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, fmt.Errorf("error in commit file traversal in commit %s, %v", commitHash, err)
+		}
+		files = append(files, file.Name)
+	}
+
+	return files, nil
 }
 
 func filesForCommit(commit string) []string {
