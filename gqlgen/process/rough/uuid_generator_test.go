9bc77ddd8ff2e5a2657a70db911b64fd88975c4a
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-22 07:43:09 +0900

    uuid test

---
+++ gqlgen/process/rough/uuid_generator_test.go
@@ -0,0 +1,41 @@
+package rough_test
+
+import (
+	"testing"
+
+	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/internal"
+	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/process/rough"
+)
+
+func TestUUIDGenerator(t *testing.T) {
+	cases := []struct {
+		roughStepFile string
+		subId         string
+		expectedUUID  string
+	}{
+		{"testdata/rough-steps/terminal1.json", "terminalCommandStep", "73388488-7f44-4617-9d6f-5cacad5bcaf8"},
+		{"testdata/rough-steps/terminal1.json", "fileTreeStep", "96d5c1df-5488-4601-9ed2-5387f3e8d5f8"},
+		{"testdata/rough-steps/terminal1.json", "openFileStep-0", "d748e6d7-124a-4889-92eb-6a2226272ea8"},
+	}
+
+	targetFile := "testdata/target-detailed-steps.json"
+	gen, err := rough.NewUUIDGenerator(targetFile)
+	if err != nil {
+		t.Fatalf("failed to create UUIDGenerator: %v", err)
+	}
+
+	for _, c := range cases {
+		t.Run(c.roughStepFile, func(t *testing.T) {
+			var roughStep rough.RoughStep
+			err := internal.JsonRead2(c.roughStepFile, &roughStep)
+			if err != nil {
+				t.Fatalf("failed to unmarshal json: %v", err)
+			}
+
+			id := gen.FindOrGenerateUUID(&roughStep, c.subId)
+			if id != c.expectedUUID {
+				t.Fatalf("expected %s, but got %s", c.expectedUUID, id)
+			}
+		})
+	}
+}
