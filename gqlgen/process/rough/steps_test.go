57bc86bb9cd09a5c87eac1afe8a4518c27206d55
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-27 04:14:33 +0900

    split test func

--- gqlgen/process/rough/steps_test.go
+++ gqlgen/process/rough/steps_test.go
@@ -11,15 +11,144 @@ import (
 )
 
 func TestTerminalSteps(t *testing.T) {
+	repoUrl := "https://github.com/richardimaoka/article-gqlgen-getting-started"
+	repo, err := test_util.GitOpenOrClone(repoUrl)
+	if err != nil {
+		t.Fatalf("cannot clone repo %s, %s", repoUrl, err)
+	}
+
+	cases := []struct {
+		roughStepFile string
+		goldenFile    string
+		InnerState    *rough.InnerState
+	}{
+		{"testdata/rough-steps/terminal1.json", "testdata/golden/terminal1.json", rough.PredictableInnerState("Terminal", "", repo)},
+		{"testdata/rough-steps/terminal2.json", "testdata/golden/terminal2.json", rough.PredictableInnerState("Terminal", "", repo)},
+		{"testdata/rough-steps/terminal3.json", "testdata/golden/terminal3.json", rough.PredictableInnerState("Terminal", "", repo)},
+		{"testdata/rough-steps/terminal4.json", "testdata/golden/terminal4.json", rough.PredictableInnerState("Terminal", "", repo)},
+	}
+
+	for _, c := range cases {
+		t.Run(c.roughStepFile, func(t *testing.T) {
+			// read rough step from file
+			var roughStep rough.RoughStep
+			err := internal.JsonRead2(c.roughStepFile, &roughStep)
+			if err != nil {
+				t.Fatalf("failed to unmarshal json: %v", err)
+			}
+
+			// convert to detailed step and verify
+			converted, err := c.InnerState.Conversion(&roughStep, repo)
+			if err != nil {
+				t.Fatalf("failed to convert rough step: %v", err)
+			}
+			result := rough.ToOmitEmptyStructs(converted)
+			internal.CompareWitGoldenFile(t, *updateFlag, c.goldenFile, result)
+		})
+	}
 }
 
 func TestCommitSteps(t *testing.T) {
+	repoUrl := "https://github.com/richardimaoka/article-gqlgen-getting-started"
+	repo, err := test_util.GitOpenOrClone(repoUrl)
+	if err != nil {
+		t.Fatalf("cannot clone repo %s, %s", repoUrl, err)
+	}
+
+	cases := []struct {
+		roughStepFile string
+		goldenFile    string
+		InnerState    *rough.InnerState
+	}{
+		{"testdata/rough-steps/manual1.json", "testdata/golden/manual1.json", rough.PredictableInnerState("Terminal", "", repo)},
+	}
+
+	for _, c := range cases {
+		t.Run(c.roughStepFile, func(t *testing.T) {
+			// read rough step from file
+			var roughStep rough.RoughStep
+			err := internal.JsonRead2(c.roughStepFile, &roughStep)
+			if err != nil {
+				t.Fatalf("failed to unmarshal json: %v", err)
+			}
+
+			// convert to detailed step and verify
+			converted, err := c.InnerState.Conversion(&roughStep, repo)
+			if err != nil {
+				t.Fatalf("failed to convert rough step: %v", err)
+			}
+			result := rough.ToOmitEmptyStructs(converted)
+			internal.CompareWitGoldenFile(t, *updateFlag, c.goldenFile, result)
+		})
+	}
 }
 
 func TestSourceErrorSteps(t *testing.T) {
+	repoUrl := "https://github.com/richardimaoka/article-gqlgen-getting-started"
+	repo, err := test_util.GitOpenOrClone(repoUrl)
+	if err != nil {
+		t.Fatalf("cannot clone repo %s, %s", repoUrl, err)
+	}
+
+	cases := []struct {
+		roughStepFile string
+		goldenFile    string
+		InnerState    *rough.InnerState
+	}{
+		{"testdata/rough-steps/source_error1.json", "testdata/golden/source_error1.json", rough.PredictableInnerState("Source Code", "", repo)},
+	}
+
+	for _, c := range cases {
+		t.Run(c.roughStepFile, func(t *testing.T) {
+			// read rough step from file
+			var roughStep rough.RoughStep
+			err := internal.JsonRead2(c.roughStepFile, &roughStep)
+			if err != nil {
+				t.Fatalf("failed to unmarshal json: %v", err)
+			}
+
+			// convert to detailed step and verify
+			converted, err := c.InnerState.Conversion(&roughStep, repo)
+			if err != nil {
+				t.Fatalf("failed to convert rough step: %v", err)
+			}
+			result := rough.ToOmitEmptyStructs(converted)
+			internal.CompareWitGoldenFile(t, *updateFlag, c.goldenFile, result)
+		})
+	}
 }
 
 func TestBrowserSteps(t *testing.T) {
+	repoUrl := "https://github.com/richardimaoka/article-gqlgen-getting-started"
+	repo, err := test_util.GitOpenOrClone(repoUrl)
+	if err != nil {
+		t.Fatalf("cannot clone repo %s, %s", repoUrl, err)
+	}
+
+	cases := []struct {
+		roughStepFile string
+		goldenFile    string
+		InnerState    *rough.InnerState
+	}{}
+
+	for _, c := range cases {
+		t.Run(c.roughStepFile, func(t *testing.T) {
+			// 1. read rough step from file
+			var roughStep rough.RoughStep
+			err := internal.JsonRead2(c.roughStepFile, &roughStep)
+			if err != nil {
+				t.Fatalf("failed to unmarshal json: %v", err)
+			}
+
+			// 3. convert to detailed step and verify
+			converted, err := c.InnerState.Conversion(&roughStep, repo)
+			if err != nil {
+				t.Fatalf("failed to convert rough step: %v", err)
+			}
+			result := rough.ToOmitEmptyStructs(converted)
+			internal.CompareWitGoldenFile(t, *updateFlag, c.goldenFile, result)
+		})
+	}
 }
 
 func TestRoughSteps(t *testing.T) {
