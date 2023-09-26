c2fcbaf3fc7567495ba24b65efdc5b6d88a45476
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-26 14:22:08 +0900

    remove unnecessary struct

--- gqlgen/process/rough/struct_test.go
+++ gqlgen/process/rough/struct_test.go
@@ -30,33 +30,6 @@ func TestDetailedStepStruct(t *testing.T) {
 	}
 }
 
-func TestDetailedStepReadStruct(t *testing.T) {
-	// production DetailedStep struct
-	ds := rough.DetailedStep{}
-	fields1 := reflect.VisibleFields(reflect.TypeOf(ds))
-	m1 := make(map[string]string)
-	for _, field := range fields1 {
-		if field.Type.String() == "bool" {
-			m1[field.Name] = "string"
-		} else {
-			m1[field.Name] = field.Type.String()
-		}
-	}
-
-	// read DetaieldStep struct
-	dsRead := rough.DetailedStepRead{}
-	fields2 := reflect.VisibleFields(reflect.TypeOf(dsRead))
-	m2 := make(map[string]string)
-	for _, field := range fields2 {
-
-		m2[field.Name] = field.Type.String()
-	}
-
-	if diff := cmp.Diff(m1, m2); diff != "" {
-		t.Fatalf("mismatch (-expected +result):\n%s", diff)
-	}
-}
-
 func TestBoolStringConversion(t *testing.T) {
 	rough.ConvertBoolean(
 		"testdata/detailed-steps/from-csv.json",
