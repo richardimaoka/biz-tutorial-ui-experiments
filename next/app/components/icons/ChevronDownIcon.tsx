863aad29e9c8ef6eb30a12bfb75690535b445ba2
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-10-16 18:19:53 +0900

    fix chevron icons

--- next/app/components/icons/ChevronDownIcon.tsx
+++ next/app/components/icons/ChevronDownIcon.tsx
@@ -1,11 +1,11 @@
-import { faChevronCircleUp } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./style.module.css";
 
 export function ChevronDownIcon() {
   return (
     <div className={styles.icon}>
-      <FontAwesomeIcon icon={faChevronCircleUp} />
+      <FontAwesomeIcon icon={faChevronDown} />
     </div>
   );
 }
