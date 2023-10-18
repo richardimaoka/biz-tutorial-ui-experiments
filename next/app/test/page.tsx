7cff99bb9687ec75bb5777683abc3da41bc14bcb
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-10-18 10:08:35 +0900

    subdir links corrected

--- next/app/test/page.tsx
+++ next/app/test/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 import { promises as fs } from "fs";
 
 export default async function Page() {
+  const routingPath = "/test";
   const cwd = process.cwd();
-  const pwd = `${cwd}/app/test`;
+  const pwd = `${cwd}/app` + routingPath;
 
   const subDirEnts = await fs.readdir(pwd, { withFileTypes: true });
   const subDirs = subDirEnts
@@ -14,7 +15,10 @@ export default async function Page() {
     <ul style={{ margin: "40px" }}>
       {subDirs.map((l) => (
         <li key={l} style={{ marginBottom: "10px", fontSize: "20px" }}>
-          <Link href={l} style={{ color: "blue", textDecoration: "underline" }}>
+          <Link
+            href={routingPath + "/" + l}
+            style={{ color: "blue", textDecoration: "underline" }}
+          >
             {l}
           </Link>
         </li>
--- next/app/test/tutorial/page.tsx
+++ next/app/test/tutorial/page.tsx
@@ -1,18 +1,28 @@
-import { TutorialComponent } from "@/app/components/tutorial/TutorialComponent";
-import { ColumnName } from "@/app/components/tutorial/definitions";
+import Link from "next/link";
+import { promises as fs } from "fs";
 
 export default async function Page() {
-  const tabs = [
-    { href: "/", name: "Terminal" as ColumnName },
-    { href: "/", name: "SourceCode" as ColumnName },
-    { href: "/", name: "Browser" as ColumnName },
-  ];
+  const routingPath = "/test/tutorial";
+  const cwd = process.cwd();
+  const pwd = `${cwd}/app` + routingPath;
+
+  const subDirEnts = await fs.readdir(pwd, { withFileTypes: true });
+  const subDirs = subDirEnts
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
 
   return (
-    <TutorialComponent tabs={tabs} selectColumn="SourceCode" columns={[]}>
-      <div
-        style={{ backgroundColor: "white", height: "90%", margin: "100px" }}
-      />
-    </TutorialComponent>
+    <ul style={{ margin: "40px" }}>
+      {subDirs.map((l) => (
+        <li key={l} style={{ marginBottom: "10px", fontSize: "20px" }}>
+          <Link
+            href={routingPath + "/" + l}
+            style={{ color: "blue", textDecoration: "underline" }}
+          >
+            {l}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 }
