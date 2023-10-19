83dad7af3a80f1aa8ef83f31447adf2f195581bb
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-10-20 03:23:08 +0900

    update editor

--- next/app/components/editor/EditorEditable.tsx
+++ next/app/components/editor/EditorEditable.tsx
@@ -3,21 +3,30 @@
 import { useEffect } from "react";
 import { EditorBare } from "./EditorBare";
 import { useEditorInstance } from "./useEditorInstance";
+// import { editor } from "monaco-editor";
 
 interface Props {
   editorText: string;
-  launguage: string;
+  language: string;
 }
 
 export function EditorEditable(props: Props) {
   const [editorInstance, onDidMount] = useEditorInstance();
 
+  // update editorText
   useEffect(() => {
-    console.log("EditorEditable useEffect");
     if (editorInstance) {
       editorInstance.setValue(props.editorText);
     }
   }, [editorInstance, props.editorText]);
+
+  // // update language
+  // useEffect(() => {
+  //   const model = editorInstance?.getModel();
+  //   if (model) {
+  //     editor.setModelLanguage(model, props.language);
+  //   }
+  // }, [editorInstance, props.language]);
 
   return <EditorBare onDidMount={onDidMount} />;
 }
