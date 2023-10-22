230df72086d520f5a641333417b95033a2c0c551
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-10-23 00:37:06 +0900

    hooks cleaned up

--- next/app/components/sourcecode2/editor/internal/EditorInnerSimpleOnlyDynamicallyImportable.tsx
+++ next/app/components/sourcecode2/editor/internal/EditorInnerSimpleOnlyDynamicallyImportable.tsx
@@ -6,10 +6,10 @@
 // !!!!
 import { editor } from "monaco-editor";
 import { EditorBare } from "./EditorBare";
+import { useEditSequence } from "./edits/useEditSequence";
 import { useEditorInstance } from "./useEditorInstance";
-import { useLanguageUpdate } from "./useLanguageUpdate";
 import { useEditorTextUpdate } from "./useEditorTextUpdate";
-import { useEffect, useRef } from "react";
+import { useLanguageUpdate } from "./useLanguageUpdate";
 
 interface Props {
   editorText: string;
@@ -21,93 +21,15 @@ interface Props {
   };
 }
 
-function executeEditCallback(
-  editorInstance: editor.IStandaloneCodeEditor,
-  editCallback: () => void
-) {
-  try {
-    editorInstance.updateOptions({ readOnly: false });
-    editCallback();
-  } finally {
-    editorInstance.updateOptions({ readOnly: true });
-  }
-}
-
-function executeEditsOneshot(
-  editorInstance: editor.IStandaloneCodeEditor,
-  edits: editor.IIdentifiedSingleEditOperation[]
-) {
-  executeEditCallback(editorInstance, () => {
-    const result = editorInstance.executeEdits("", edits);
-    if (!result) {
-      // TODO: throw error to trigger error.tsx
-      console.log("executeEdits for monaco editor failed!");
-    }
-  });
-}
-
-function executeEditsAnimation(
-  editorInstance: editor.IStandaloneCodeEditor,
-  edits: editor.IIdentifiedSingleEditOperation[]
-) {
-  const setTimeoutInterval = 20; // milliseconds
-
-  function executeAtomicEdit(at: number) {
-    executeEditCallback(editorInstance, () => {
-      const result = editorInstance.executeEdits("", [edits[at]]);
-      if (!result) {
-        // TODO: throw error to trigger error.tsx
-        console.log("executeEdits for monaco editor failed!");
-      }
-    });
-
-    if (at < edits.length - 1) {
-      window.setTimeout(() => executeAtomicEdit(at + 1), setTimeoutInterval);
-    }
-  }
-
-  window.setTimeout(() => {
-    executeAtomicEdit(0);
-  }, setTimeoutInterval);
-}
-
 // `default` export, for easier use with Next.js dynamic import
 export default function EditorInnerOnlyDynamicallyImportable(props: Props) {
   const [editorInstance, onDidMount] = useEditorInstance();
+
   useEditorTextUpdate(editorInstance, props.editorText);
+
   useLanguageUpdate(editorInstance, props.language);
 
-  const isEditsMade = useRef(false);
-
-  // Execute edits
-  useEffect(() => {
-    if (editorInstance) {
-      const edits = props.editSequence?.edits;
-      if (edits && edits.length > 0) {
-        // clear previous edits upon props change
-        if (isEditsMade) {
-          executeEditCallback(editorInstance, () => {
-            editorInstance.trigger("", "undo", null);
-          });
-        }
-
-        if (props.editSequence?.animate) {
-          executeEditsAnimation(editorInstance, edits);
-        } else {
-          executeEditsOneshot(editorInstance, edits);
-        }
-        isEditsMade.current = true;
-      } else {
-        // clear edits if edits is undefined or []
-        editorInstance.updateOptions({ readOnly: false });
-
-        editorInstance.trigger("", "undo", null);
-        isEditsMade.current = false;
-
-        editorInstance.updateOptions({ readOnly: true });
-      }
-    }
-  }, [editorInstance, props.editSequence]);
+  useEditSequence(editorInstance, props.editSequence);
 
   return <EditorBare onDidMount={onDidMount} />;
 }
