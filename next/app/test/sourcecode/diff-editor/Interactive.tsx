bc67a1c406def5f564a2ca8bbc18456db3cd1567
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-10-27 04:08:01 +0900

    switch diff and non-diff smoothly

--- next/app/test/sourcecode/diff-editor/Interactive.tsx
+++ next/app/test/sourcecode/diff-editor/Interactive.tsx
@@ -12,27 +12,52 @@ interface Props {
 }
 
 export function Interactive(props: Props) {
-  const [index, setIndex] = useState(1);
-
+  const [showDiff, setShowDiff] = useState(false);
   return (
-    <div style={{ height: "100%" }} onClick={() => setIndex((index + 1) % 2)}>
-      <Carousel columnWidth={700} currentIndex={index}>
-        <div style={{ display: "flex", height: "100%", width: "1400px" }}>
-          <div style={{ width: "700px" }}>
-            <EditorEditable
-              editorText={props.modified}
-              language={props.language}
-            />
-          </div>
-          <div style={{ width: "700px" }}>
-            <DiffEditorBare
-              original={props.original}
-              modified={props.modified}
-              language={props.language}
-            />
-          </div>
-        </div>
-      </Carousel>
+    <div style={{ height: "100%" }}>
+      <button
+        onClick={() => {
+          setShowDiff(!showDiff);
+        }}
+      >
+        switch
+      </button>
+      <div style={{ height: "100%", display: showDiff ? "none" : "block" }}>
+        <DiffEditorBare
+          original={props.original}
+          modified={props.modified}
+          language={props.language}
+        />
+      </div>
+      <div style={{ height: "100%", display: showDiff ? "block" : "none" }}>
+        <EditorEditable editorText={props.modified} language={props.language} />
+      </div>
     </div>
   );
 }
+
+// export function Interactive(props: Props) {
+//   const [index, setIndex] = useState(1);
+
+//   return (
+//     <div style={{ height: "100%" }} onClick={() => setIndex((index + 1) % 2)}>
+//       <Carousel columnWidth={700} currentIndex={index}>
+//         <div style={{ display: "flex", height: "100%", width: "1400px" }}>
+//           <div style={{ width: "700px" }}>
+//             <EditorEditable
+//               editorText={props.modified}
+//               language={props.language}
+//             />
+//           </div>
+//           <div style={{ width: "700px" }}>
+//             <DiffEditorBare
+//               original={props.original}
+//               modified={props.modified}
+//               language={props.language}
+//             />
+//           </div>
+//         </div>
+//       </Carousel>
+//     </div>
+//   );
+// }
