9b8b0f635f3048092fbcd5d19dd9ac1f410305fc
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-10-27 04:02:20 +0900

    carousel

--- next/app/test/sourcecode/diff-editor/Interactive.tsx
+++ next/app/test/sourcecode/diff-editor/Interactive.tsx
@@ -2,6 +2,7 @@
 
 import { DiffEditorBare } from "@/app/components/sourcecode2/diff-editor/DiffEditorBare";
 import { EditorEditable } from "@/app/components/sourcecode2/editor/EditorEditable";
+import { Carousel } from "@/app/components/tutorial/carousel/Carousel";
 import { useState } from "react";
 
 interface Props {
@@ -11,26 +12,27 @@ interface Props {
 }
 
 export function Interactive(props: Props) {
-  const [showDiff, setShowDiff] = useState(false);
+  const [index, setIndex] = useState(1);
 
   return (
-    <div style={{ height: "100%" }}>
-      <button
-        onClick={() => {
-          setShowDiff(!showDiff);
-        }}
-      >
-        switch
-      </button>
-      {showDiff ? (
-        <DiffEditorBare
-          original={props.original}
-          modified={props.modified}
-          language={props.language}
-        />
-      ) : (
-        <EditorEditable editorText={props.modified} language={props.language} />
-      )}
+    <div style={{ height: "100%" }} onClick={() => setIndex((index + 1) % 2)}>
+      <Carousel columnWidth={700} currentIndex={index}>
+        <div style={{ display: "flex", height: "100%", width: "1400px" }}>
+          <div style={{ width: "700px" }}>
+            <EditorEditable
+              editorText={props.modified}
+              language={props.language}
+            />
+          </div>
+          <div style={{ width: "700px" }}>
+            <DiffEditorBare
+              original={props.original}
+              modified={props.modified}
+              language={props.language}
+            />
+          </div>
+        </div>
+      </Carousel>
     </div>
   );
 }
