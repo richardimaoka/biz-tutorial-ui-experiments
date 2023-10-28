0fe299add737604394a1b318bb3602103d88c85f
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-10-29 03:23:44 +0900

    source code input

--- gqlgen/process/input/source_code.go
+++ gqlgen/process/input/source_code.go
@@ -70,15 +70,92 @@ func toSourceCodeCommit(ab *Abstract) (*SourceCodeCommit, error) {
 }
 
 type SourceCodeOpen struct {
-	Comment  string
-	FilePath string
-	Tooltip  *SourceCodeTooltip
+	StepId   string             `json:"stepId"`
+	Comment  string             `json:"comment"`
+	FilePath string             `json:"filePath"`
+	Tooltip  *SourceCodeTooltip `json:"tooltip"`
+}
+
+func toSourceCodeOpen(ab *Abstract) (*SourceCodeOpen, error) {
+	errorPrefix := "failed to convert to SourceCodeOpen"
+
+	//
+	// Check column and type
+	//
+	if strings.ToLower(ab.Column) != "source" {
+		return nil, fmt.Errorf("%s, called for wrong 'column' = %s", errorPrefix, ab.Column)
+	}
+	if ab.Type != "" && strings.ToLower(ab.Type) != "commit" {
+		return nil, fmt.Errorf("%s, called for wrong 'type' = %s", errorPrefix, ab.Type)
+	}
+
+	//
+	// Check instruction fields
+	//
+	if ab.Instruction == "" {
+		return nil, fmt.Errorf("%s, 'instruction' is empty", errorPrefix)
+	}
+
+	//
+	// Check tooltip fields
+	//
+	tooltip, err := toSourceCodeTooltip(ab)
+	if err != nil {
+		return nil, fmt.Errorf("%s, %s", errorPrefix, err)
+	}
+
+	return &SourceCodeOpen{
+		StepId:   ab.StepId,
+		Comment:  ab.Comment,
+		FilePath: ab.Instruction,
+		Tooltip:  tooltip,
+	}, nil
 }
 
 type SourceCodeError struct {
-	Comment  string
-	FilePath string
-	Tooltip  *SourceCodeTooltip
+	StepId   string             `json:"stepId"`
+	Comment  string             `json:"comment"`
+	FilePath string             `json:"filePath"`
+	Tooltip  *SourceCodeTooltip `json:"tooltip"`
+}
+
+func toSourceCodeError(ab *Abstract) (*SourceCodeError, error) {
+	errorPrefix := "failed to convert to SourceCodeError"
+
+	//
+	// Check column and type
+	//
+	if strings.ToLower(ab.Column) != "source" {
+		return nil, fmt.Errorf("%s, called for wrong 'column' = %s", errorPrefix, ab.Column)
+	}
+	if ab.Type != "" && strings.ToLower(ab.Type) != "commit" {
+		return nil, fmt.Errorf("%s, called for wrong 'type' = %s", errorPrefix, ab.Type)
+	}
+
+	//
+	// Check instruction fields
+	//
+	if ab.Instruction == "" {
+		return nil, fmt.Errorf("%s, 'instruction' is empty", errorPrefix)
+	}
+
+	//
+	// Check tooltip fields
+	//
+	tooltip, err := toSourceCodeTooltip(ab)
+	if err != nil {
+		return nil, fmt.Errorf("%s, %s", errorPrefix, err)
+	}
+	if tooltip == nil {
+		return nil, fmt.Errorf("%s, source code error needs the error detail in 'tooltip'", errorPrefix)
+	}
+
+	return &SourceCodeError{
+		StepId:   ab.StepId,
+		Comment:  ab.Comment,
+		FilePath: ab.Instruction,
+		Tooltip:  tooltip,
+	}, nil
 }
 
 func toSourceCodeTooltip(ab *Abstract) (*SourceCodeTooltip, error) {
