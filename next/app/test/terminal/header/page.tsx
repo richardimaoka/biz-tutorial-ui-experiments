86e4ea97ebadc3347ca2894a19870cce45fb6864
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-10-30 05:23:05 +0900

    use FragmentType

--- next/app/test/terminal/header/page.tsx
+++ next/app/test/terminal/header/page.tsx
@@ -1,28 +1,27 @@
 import { TerminalHeaderGql } from "@/app/components/terminal2/header/TerminalHeaderGql";
 import { TerminalHeaderGqlFragment } from "@/libs/gql/graphql";
+import { TypedDocumentNode } from "@graphql-typed-document-node/core";
+
+import { FragmentType } from "@/libs/gql";
+
+type b = FragmentType<TypedDocumentNode<TerminalHeaderGqlFragment, unknown>>;
+
+function typeCast(f: TerminalHeaderGqlFragment): b {
+  return {
+    " $fragmentRefs": {
+      TerminalHeaderGqlFragment: f,
+    },
+    ...f,
+  };
+}
 
 export default function Page() {
+  type TT = TypedDocumentNode<TerminalHeaderGqlFragment, unknown>;
+
   const fragmentt: TerminalHeaderGqlFragment = {
     __typename: "Terminal2",
     currentDirectory: "dirdir",
   };
-
-  type a = {
-    " $fragmentRefs"?:
-      | {
-          TerminalHeaderGqlFragment: TerminalHeaderGqlFragment;
-        }
-      | undefined;
-  };
-
-  function typeCast(f: TerminalHeaderGqlFragment): a {
-    return {
-      " $fragmentRefs": {
-        TerminalHeaderGqlFragment: f,
-      },
-      ...f,
-    };
-  }
 
   console.log(typeCast(fragmentt));
 
