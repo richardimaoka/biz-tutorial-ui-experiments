e031d0566135e009e09af42c24818fa24cba39ad
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-11-04 14:48:54 +0900

    remove unnecessary tests

--- next/app/test/sourcecode/tooltip/height/Child.tsx
+++
@@ -1,9 +0,0 @@
-import { EditorTooltip } from "@/app/components/sourcecode2/tooltip/EditorTooltip";
-
-interface Props {
-  markdownBody: string;
-}
-
-export function Child(props: Props) {
-  return <div>{<EditorTooltip markdownBody={props.markdownBody} />}</div>;
-}
