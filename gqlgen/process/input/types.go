809734bfc8282aff53247c3ef5c398545f09ad8f
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-11-07 03:53:17 +0900

    types.go

--- gqlgen/process/input/types.go
+++ gqlgen/process/input/types.go
@@ -1,5 +1,39 @@
 package input
 
+type ColumnType = string
+
+const (
+	// Lower cases since they are from manual entries
+	SourceType   ColumnType = "source"
+	TerminalType ColumnType = "terminal"
+	BrowserType  ColumnType = "browser"
+)
+
+type SubType = string
+
+const (
+	// Lower cases since they are from manual entries
+	CommandSubType SubType = "command"
+	OutputSubType  SubType = "output"
+)
+
+type Row struct {
+	StepId        string `json:"stepId"`
+	Phase         string `json:"phase"`
+	Comment       string `json:"comment"`
+	Column        string `json:"column"` //not Column but string, because it's input from manual entry, not sanitized
+	Type          string `json:"type"`
+	Trivial       string `json:"trivial"`
+	Instruction   string `json:"instruction"`
+	Instruction2  string `json:"instruction2"`
+	Instruction3  string `json:"instruction3"`
+	ModalText     string `json:"modalText"`
+	Tooltip       string `json:"tooltip"`
+	TooltipTiming string `json:"tooltipTiming"`
+	TooltipLine   int    `json:"tooltipLine"`
+	// TooltipPosition string `json:"tooltipPosition"`
+}
+
 type ResultStep struct {
 	// Uppercase fields to allow json dump for testing
 
