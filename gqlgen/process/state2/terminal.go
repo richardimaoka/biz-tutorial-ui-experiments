907cb8648dee96a655dceab6c0caa9aff95bd3a6
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-11-10 20:50:58 +0900

    terminal column

--- gqlgen/process/state2/terminal.go
+++ gqlgen/process/state2/terminal.go
@@ -7,7 +7,7 @@ import (
 )
 
 /**
- * Terminal Tooltip types
+ * Terminal Tooltip type and methods
  */
 type TerminalTooltipTiming string
 
@@ -32,8 +32,16 @@ type TerminalTooltip struct {
 	timing       TerminalTooltipTiming
 }
 
+func (t *TerminalTooltip) ToGraphQL() *model.TerminalTooltip2 {
+	timing := t.timing.ToGraphQL()
+	return &model.TerminalTooltip2{
+		Timing:       &timing,
+		MarkdownBody: t.markdownBody,
+	}
+}
+
 /**
- * Terminal Entry types
+ * Terminal Entry type and methods
  */
 type TerminalEntryType string
 
@@ -71,27 +79,38 @@ func (p *TerminalEntry) ToGraphQLTerminalEntry() *model.TerminalEntry {
 }
 
 /**
- * Terminal types
+ * Terminal type and methods
  */
 
 type Terminal struct {
-	step             string
 	terminalName     string
 	currentDirectory string
 	entries          []TerminalEntry
 	tooltip          *TerminalTooltip
 }
 
-func NewTerminal() *Terminal {
-	return &Terminal{}
+func NewTerminal(name string) *Terminal {
+	return &Terminal{terminalName: name}
 }
 
-func (t *Terminal) AppendEntry(entryType TerminalEntryType, id, text string) {
+func (t *Terminal) WriteCommand(id, command string) {
 	t.entries = append(t.entries, TerminalEntry{
 		id:        id,
-		entryType: entryType,
-		text:      text,
+		entryType: Command,
+		text:      command,
 	})
+}
+
+func (t *Terminal) WriteOutput(id, output string) {
+	t.entries = append(t.entries, TerminalEntry{
+		id:        id,
+		entryType: Command,
+		text:      output,
+	})
+}
+
+func (t *Terminal) ChangeCurrentDirectory(dirPath string) {
+	t.currentDirectory = dirPath
 }
 
 func (t *Terminal) SetTooltip(tooltipContents string, timing TerminalTooltipTiming) {
@@ -105,11 +124,7 @@ func (t *Terminal) ClearTooltip() {
 	t.tooltip = nil
 }
 
-func (t *Terminal) ChangeCurrentDirectory(dirPath string) {
-	t.currentDirectory = dirPath
-}
-
-func (t *Terminal) ToGraphQLTerminal() *model.Terminal2 {
+func (t *Terminal) ToGraphQL() *model.Terminal2 {
 	// copy to avoid mutation effect afterwards
 	terminalName := stringRef(t.terminalName)
 
@@ -122,19 +137,101 @@ func (t *Terminal) ToGraphQLTerminal() *model.Terminal2 {
 		})
 	}
 
-	var modelTooltip *model.TerminalTooltip2
-	if t.tooltip != nil {
-		timing := t.tooltip.timing.ToGraphQL()
-		modelTooltip = &model.TerminalTooltip2{
-			MarkdownBody: t.tooltip.markdownBody,
-			Timing:       &timing,
-		}
-	}
-
-	return &model.Terminal2{
+	m := model.Terminal2{
 		Name:             terminalName,
 		CurrentDirectory: t.currentDirectory,
 		Entries:          modelEntries,
-		Tooltip:          modelTooltip,
+	}
+	if t.tooltip != nil {
+		m.Tooltip = t.tooltip.ToGraphQL()
+	}
+
+	return &m
+}
+
+/**
+ * Terminal Column type and methods
+ */
+
+type TerminalColumn struct {
+	terminals []*Terminal
+}
+
+func (c *TerminalColumn) WriteCommand(
+	stepId string,
+	name string,
+	command string,
+	tooltipContents string,
+) {
+	var terminal *Terminal
+	for _, t := range c.terminals {
+		if t.terminalName == name {
+			terminal = t
+		}
+	}
+	if terminal == nil {
+		terminal = NewTerminal(name)
+		c.terminals = append(c.terminals, terminal)
+	}
+
+	terminal.WriteCommand(stepId, command)
+	if tooltipContents == "" {
+		terminal.ClearTooltip()
+	} else {
+		terminal.SetTooltip(tooltipContents, START)
+	}
+}
+
+func (c *TerminalColumn) WriteOutput(
+	stepId string,
+	name string,
+	output string,
+	tooltipContents string,
+) {
+	var terminal *Terminal
+	for _, t := range c.terminals {
+		if t.terminalName == name {
+			terminal = t
+		}
+	}
+	if terminal == nil {
+		terminal = NewTerminal(name)
+		c.terminals = append(c.terminals, terminal)
+	}
+
+	terminal.WriteOutput(stepId, output)
+	if tooltipContents == "" {
+		terminal.ClearTooltip()
+	} else {
+		terminal.SetTooltip(tooltipContents, START)
+	}
+}
+
+func (c *TerminalColumn) ChangeCurrentDirectory(
+	name string,
+	dirPath string,
+) {
+	var terminal *Terminal
+	for _, t := range c.terminals {
+		if t.terminalName == name {
+			terminal = t
+		}
+	}
+	if terminal == nil {
+		terminal = NewTerminal(name)
+		c.terminals = append(c.terminals, terminal)
+	}
+
+	terminal.ChangeCurrentDirectory(dirPath)
+}
+
+func (c *TerminalColumn) ToGraphQL() *model.TerminalColumn2 {
+	var terminals []*model.Terminal2
+	for _, t := range c.terminals {
+		terminals = append(terminals, t.ToGraphQL())
+	}
+
+	return &model.TerminalColumn2{
+		Terminals: terminals,
 	}
 }
