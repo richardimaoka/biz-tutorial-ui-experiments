2a3095666a9d03381651b77254b7b8bbdea2eb9d
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-11-11 23:53:27 +0900

    rough and commits dirs removed

--- gqlgen/main.go
+++ gqlgen/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
-	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/process/commits"
 	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/process/input"
-	"github.com/richardimaoka/biz-tutorial-ui-experiments/gqlgen/process/rough"
 )
 
 func main() {
@@ -21,26 +18,6 @@ func main() {
 	switch subCmd {
 	case "input":
 		input.Run(os.Args[2:])
-	case "rough":
-		if len(os.Args) != 3 {
-			panic("rough sub command needs extra argument")
-		}
-
-		tutorial := os.Args[2]
-		err := rough.Process("data/"+tutorial, fmt.Sprintf("https://github.com/richardimaoka/article-%s.git", tutorial))
-		if err != nil {
-			panic(err)
-		}
-	case "csv":
-		err := rough.ConvertBoolean("data/gqlgen-getting-started/steps2.json", "data/gqlgen-getting-started/steps3.json")
-		if err != nil {
-			panic(err)
-		}
-	case "commits":
-		err := commits.Committtssss("gqlgen-getting-started")
-		if err != nil {
-			panic(err)
-		}
 	case "server":
 		Server()
 	default:
