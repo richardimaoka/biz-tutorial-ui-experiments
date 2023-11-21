72fe5fe76d092246dd9c593482a704629f67765b
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-11-22 03:56:50 +0900

    copy page.tsx to [tutorial]

---
+++ next/app/[tutorial]/page.tsx
@@ -0,0 +1,34 @@
+import { GqlTutorialComponent } from "@/app/components/tutorial/GqlTutorialComponent";
+import { graphql } from "@/libs/gql";
+import { request } from "graphql-request";
+
+const queryDefinition = graphql(`
+  query appTestTutorialColumnsPage {
+    _test {
+      appTestTutorialColumnsPage {
+        ...GqlTutorialComponent
+      }
+    }
+  }
+`);
+
+interface PageParams {
+  searchParams: {};
+}
+
+export default async function Page({ searchParams }: PageParams) {
+  // const variables = { step: stepNum };
+  const data = await request(
+    "http://localhost:8080/query",
+    queryDefinition
+    // variables
+  );
+
+  const fragment = data._test?.appTestTutorialColumnsPage;
+
+  return (
+    <div style={{ height: "100svh" }}>
+      {fragment && <GqlTutorialComponent fragment={fragment} />}
+    </div>
+  );
+}
