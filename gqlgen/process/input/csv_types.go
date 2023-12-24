7449d23cbcec0994d6fbb48d1657f571ee26c146
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-12-24 22:41:51 +0900

    csv types

--- gqlgen/process/input/csv_types.go
+++ gqlgen/process/input/csv_types.go
@@ -6,24 +6,51 @@ import (
 	"strconv"
 )
 
-type CsvString string
+type CsvString string //Whether it's an integer value or a string, forcefully convert to a String in Go
 
 func (v *CsvString) UnmarshalJSON(b []byte) error {
-
 	var intValue int
-	err1 := json.Unmarshal(b, &intValue)
-	// if no error, successfully unmarshaled to int
-	if err1 == nil {
+	if err := json.Unmarshal(b, &intValue); err == nil {
+		// if no error, successfully unmarshaled to int, then convert the int to CsvString
 		*v = CsvString(strconv.Itoa(intValue))
 		return nil
 	}
 
 	var stringValue string
-	err2 := json.Unmarshal(b, &stringValue)
-	if err2 == nil {
+	if err := json.Unmarshal(b, &stringValue); err == nil {
+		// if no error, successfully unmarshaled to string
 		*v = CsvString(stringValue)
+		return nil
+	} else {
+		return fmt.Errorf("unmarshal to CsvString failed, %s", err)
+	}
+}
+
+type CsvInt int
+
+func (v *CsvInt) UnmarshalJSON(b []byte) error {
+	var stringValue string
+	if err := json.Unmarshal(b, &stringValue); err == nil {
+		if stringValue == "" {
+			*v = CsvInt(0)
+			return nil
+		}
+
+		intValue, err := strconv.Atoi(stringValue)
+		if err != nil {
+			return fmt.Errorf("unmarshan to CsvInt failed, %s", err)
+		}
+		*v = CsvInt(intValue)
 		return nil
 	}
 
-	return fmt.Errorf("unmarshal to CsvString failed, %s, %s", err1, err2)
+	var intValue int
+	err := json.Unmarshal(b, &intValue)
+	// if no error, successfully unmarshaled to int
+	if err == nil {
+		*v = CsvInt(intValue)
+		return nil
+	}
+
+	return fmt.Errorf("unmarshal to CsvInt failed, %s", err)
 }
--- gqlgen/process/input/csv_types_test.go
+++ gqlgen/process/input/csv_types_test.go
@@ -29,7 +29,33 @@ func TestCsvString(t *testing.T) {
 			if result != c.expected {
 				t.Fatalf("CSV string is expected = '%s' but got '%s'", c.expected, result)
 			}
+		})
+	}
+}
 
+func TestCsvInt(t *testing.T) {
+	cases := []struct {
+		name       string
+		expected   input.CsvInt
+		inputBytes []byte
+	}{
+		{"one", 1, []byte(`1`)},
+		{"two", 2, []byte(`2`)},
+		{"zero1", 0, []byte(`""`)},
+		{"zero2", 0, []byte(`"0"`)}}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			var result input.CsvInt
+			err := json.Unmarshal(c.inputBytes, &result)
+
+			if err != nil {
+				t.Fatalf("failed %s", err)
+			}
+
+			if result != c.expected {
+				t.Fatalf("CSV int is expected = '%d' but got '%d'", c.expected, result)
+			}
 		})
 	}
 }
